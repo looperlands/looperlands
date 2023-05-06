920c516149c1699298da2f05c79e1733c8747b9e
Balks Hamster <balkshamster@proton.me>
2023-05-06 13:38:42 -0400

    removed comma

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -94,7 +94,7 @@ Types = {
         // NFTs
         NFT_c762bf80c40453b66f5eb91a99a5a84731c3cc83e1bcadaa9c62e2e59e19e4f6: 70,
         NFT_38278eacc7d1c86fdbc85d798dca146fbca59a2e5e567dc15898ce2edac21f5f: 71,
-        NFT_d2fb1ad9308803ea4df2ba6b1fe0930ad4d6443b3ac6468eaedbc9e2c214e57a: 72, // @lastid@
+        NFT_d2fb1ad9308803ea4df2ba6b1fe0930ad4d6443b3ac6468eaedbc9e2c214e57a: 72 // @lastid@
         //@nextIdLine@
     },
     
