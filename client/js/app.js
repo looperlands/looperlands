470cdb37ac3bfd68b2ebd87dca46b00346951865
Balks Hamster <balkshamster@proton.me>
2023-05-11 19:05:38 -0400

    tweet test

--- client/js/app.js
+++ client/js/app.js
@@ -324,7 +324,7 @@ define(['jquery', 'storage'], function($, Storage) {
                 if(!achievement.hidden) {
                     self.setAchievementData($a, achievement.name, achievement.desc);
                 }
-                $a.find('.twitter').attr('href', 'http://twitter.com/share?url=http%3A%2F%2FLoopQuest.mozilla.org&text=I%20unlocked%20the%20%27'+ achievement.name +'%27%20achievement%20on%20Mozilla%27s%20%23LoopQuest%21&related=glecollinet:Creators%20of%20LoopQuest%2Cwhatthefranck');
+                $a.find('.twitter').attr('href', 'http://twitter.com/share?url=https%3A%2F%2Floopworms.ioF%2MetaverseF%2&text=I%20unlocked%20the%20%27'+ achievement.name +'%27%20achievement%20on%20Loopworm%27s%20%23LoopQuest%21');
                 $a.show();
                 $a.find('a').click(function() {
                      var url = $(this).attr('href');
