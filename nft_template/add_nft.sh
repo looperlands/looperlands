2b2011403e433416c7b74c40c154f3d095d8d8f8
Balks Hamster <balkshamster@proton.me>
2023-05-13 08:37:51 -0400

    changed add script

--- nft_template/add_nft.sh
+++ nft_template/add_nft.sh
@@ -22,9 +22,18 @@ NFT_ID=NFT_$MINUS_0X
 type=$3
 echo Adding NFT with id $NFT_ID, type $type
 jq ".id=\"${NFT_ID}\"" spritemap.json > ../client/sprites/$NFT_ID.json
-jq ".id=\"item-${NFT_ID}\"" itemspritemap.json > ../client/sprites/item-$NFT_ID.json
+if [ "$3" = "weapon" ]; then
+  jq ".id=\"item-${NFT_ID}\"" weaponpritemap.json > ../client/sprites/item-$NFT_ID.json
+else
+  jq ".id=\"item-${NFT_ID}\"" armorspritemap.json > ../client/sprites/item-$NFT_ID.json
+fi
+
 for i in {1..3}; do
     cp $IMAGE_DIR/$i.png ../client/img/$i/$NFT_ID.png
-    cp armor$i.png ../client/img/$i/item-$NFT_ID.png
+    if [ "$3" = "weapon" ]; then
+      cp item-$i.png ../client/img/$i/item-$NFT_ID.png
+    else
+      cp armor$i.png ../client/img/$i/item-$NFT_ID.png
+    fi
 done
 ./add_nft_js.sh $MINUS_0X $3
