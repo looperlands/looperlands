2b55bb4b5d959e5790cdb0244d6dfa0b84987852
Balks Hamster <balkshamster@proton.me>
2023-05-16 19:30:32 -0400

    remove sessionId from save

--- client/js/saveworker.js
+++ client/js/saveworker.js
@@ -2,7 +2,9 @@ importScripts("lib/axios.min.js");
 onmessage = function (e) {
     if (e.data.loopquest) {
         console.debug("saving..", e.data);
-        axios.put(`/session/${e.data.sessionId}`, e.data).then(function (response) {
+        let sessionId = e.data.sessionId;
+        delete e.data.sessionId;
+        axios.put(`/session/${sessionId}`, e.data).then(function (response) {
             console.debug("Response from Loopworms: ", response.status, response.text, response.data);
         }).catch(function (error) {
             console.log(error);
