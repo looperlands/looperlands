96ce8d929ed8081dbfad0e6fe67b168c46cdbb34
Balks Hamster <balkshamster@proton.me>
2023-05-16 09:12:03 -0400

    changed title color to white

--- client/js/renderer.js
+++ client/js/renderer.js
@@ -566,7 +566,7 @@ function(Camera, Item, Character, Player, Timer) {
                               true,
                               color);
                 if (entity.title !== undefined) {
-                    this.drawText(entity.title, (entity.x + 8) * this.scale, (entity.y + entity.nameOffsetY + 5) * this.scale, true, "#702963", 1, true);
+                    this.drawText(entity.title, (entity.x + 8) * this.scale, (entity.y + entity.nameOffsetY + 5) * this.scale, true, "white", 1, true);
                 }
             }
             this.context.restore();
