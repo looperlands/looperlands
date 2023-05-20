637e03fde8a114f54c9c0e847b97a679bd9a9a77
balkshamster <balkshamster@proton.me>
2023-05-20 15:30:10 -0400

    improved local session to start a edge session to run bq

--- local-session.sh
+++ local-session.sh
@@ -12,4 +12,4 @@ nftId=$2
 sessionId=`curl -X POST -H "Content-Type: application/json" -H "x-api-key: $LOOPWORMS_API_KEY" -d "{\"walletId\": \"$walletId\", \"nftId\" : \"$nftId\", \"title\": \"Title 🏛️\"}" http://127.0.0.1:8000/session | jq -r '.sessionId'`
 url=http://127.0.0.1:8000/?sessionId=$sessionId
 echo Navigate to the following URL to start playing: $url
-open $url
+open $url || "/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" $url
