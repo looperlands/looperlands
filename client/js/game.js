caec1fc1614b783f50ebb72bc45ccd758db8b1e4
balkshamster <balkshamster@proton.me>
2023-05-20 21:18:47 -0400

    pvp zone improvements

--- client/js/game.js
+++ client/js/game.js
@@ -1967,7 +1967,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
         	    if(entity instanceof Mob) {
         	        this.makePlayerAttack(entity);
                 } else if (entity instanceof Player) {
-                    var pvpZone = {top: {x: 14, y: 326}, bottom: {x: 39, y: 348}}
+                    var pvpZone = {top: {x: 0, y: 316}, bottom: {x: 92, y: 369}}
                     var inPvpZone = entity.gridX > pvpZone.top.x && entity.gridX < pvpZone.bottom.x && entity.gridY > pvpZone.top.y && entity.gridY < pvpZone.bottom.y;
                     if (inPvpZone) {
                         this.makePlayerAttack(entity);
