c42b603a1c1a17259bf6d224de398e7f1a05b655
balkshamster <balkshamster@proton.me>
2023-05-27 17:49:52 -0400

    experience percentage fix

--- local-session.sh
+++ local-session.sh
@@ -9,7 +9,7 @@ if [ $# -ne 2 ]; then
 fi
 walletId=$1
 nftId=$2
-sessionId=`curl -X POST -H "Content-Type: application/json" -H "x-api-key: $LOOPWORMS_API_KEY" -d "{\"walletId\": \"$walletId\", \"nftId\" : \"$nftId\", \"title\": \"Title 🏛️\", \"xp\": 500}" http://127.0.0.1:8000/session | jq -r '.sessionId'`
+sessionId=`curl -X POST -H "Content-Type: application/json" -H "x-api-key: $LOOPWORMS_API_KEY" -d "{\"walletId\": \"$walletId\", \"nftId\" : \"$nftId\", \"title\": \"Title 🏛️\", \"xp\": 25100}" http://127.0.0.1:8000/session | jq -r '.sessionId'`
 url=http://127.0.0.1:8000/?sessionId=$sessionId
 echo Navigate to the following URL to start playing: $url
 open $url || "/mnt/c/Program Files (x86)/Microsoft/Edge/Application/msedge.exe" $url
--- server/js/formulas.js
+++ server/js/formulas.js
@@ -6,60 +6,73 @@ var Formulas = {};
 const EXPERIENCE_GROWTH = 5;
 const BASE_EXPERIENCE = 1000;
 
-Formulas.dmg = function(weaponLevel, armorLevel) {
+Formulas.dmg = function (weaponLevel, armorLevel) {
     var dealt = weaponLevel * Utils.randomInt(5, 10),
         absorbed = armorLevel * Utils.randomInt(1, 3),
-        dmg =  dealt - absorbed;
-    
+        dmg = dealt - absorbed;
+
     //console.log("abs: "+absorbed+"   dealt: "+ dealt+"   dmg: "+ (dealt - absorbed));
-    if(dmg <= 0) {
+    if (dmg <= 0) {
         return Utils.randomInt(0, 3);
     } else {
         return dmg;
     }
 };
 
-Formulas.hp = function(level) {
+Formulas.hp = function (level) {
     var hp = 80 + ((level - 1) * 15);
     return hp;
 };
 
-Formulas.xp = function(mobProperties) {
-    return (Math.round(Math.round((mobProperties.hp + mobProperties.armor + mobProperties.weapon)/2)/10) * 10)/2;
+Formulas.xp = function (mobProperties) {
+    return (Math.round(Math.round((mobProperties.hp + mobProperties.armor + mobProperties.weapon) / 2) / 10) * 10) / 2;
 }
 
 
-Formulas.level = function(experience) {
-    
-    let level = 1;
-    let experienceRequired = BASE_EXPERIENCE;
-  
-    while (experience >= experienceRequired) {
-      experience -= experienceRequired;
-      level++;
-      experienceRequired = Math.floor(BASE_EXPERIENCE * Math.pow(EXPERIENCE_GROWTH, level - 1));
+
+Formulas.calculateExperienceMap = function (numLevels) {
+    const experienceMap = {};
+
+    for (let level = 1; level <= numLevels; level++) {
+        let experience = 0;
+        let experienceRequired = BASE_EXPERIENCE;
+
+        for (let currentLevel = 1; currentLevel < level; currentLevel++) {
+            experience += experienceRequired;
+            experienceRequired = Math.floor(BASE_EXPERIENCE * Math.pow(EXPERIENCE_GROWTH, currentLevel - 1));
+        }
+
+        experienceMap[level] = experienceRequired;
     }
-  
+
+    return experienceMap;
+};
+
+// Usage example:
+const numLevels = 20;
+const EXPERIENCE_MAP = Formulas.calculateExperienceMap(numLevels);
+
+Formulas.level = function (experience) {
+    let level = 1;
+
+    while (experience >= EXPERIENCE_MAP[level]) {
+        experience -= EXPERIENCE_MAP[level];
+        level++;
+    }
+
     return level;
-}
+};
+
+
 
 Formulas.calculatePercentageToNextLevel = function (experience) {
     const currentLevel = Formulas.level(experience);
-    
-    let previousExpRequired = 0;
-    if (currentLevel > 1) {
-        previousExpRequired = Math.floor(BASE_EXPERIENCE * Math.pow(EXPERIENCE_GROWTH, currentLevel - 2));
-    }
-    let experienceRequiredForCurrentLevel = Math.floor(BASE_EXPERIENCE * Math.pow(EXPERIENCE_GROWTH, currentLevel - 1));
-    experience-=previousExpRequired;
-
-    let percentage;
-    if (experience === 0) {
-        percentage = 0;
-    } else {
-        percentage = (experience/experienceRequiredForCurrentLevel) * 100;
-        percentage = Math.round(percentage, 2);
-    }
+    const currentLevelExperience = EXPERIENCE_MAP[currentLevel];
+    const nextLevelExperience = EXPERIENCE_MAP[currentLevel + 1];
+    const experienceToNextLevel = experience - currentLevelExperience;
+    const experienceRequiredToLevel = nextLevelExperience - currentLevelExperience;
+    let percentage = (experienceToNextLevel / experienceRequiredToLevel) * 100;
+    percentage = percentage.toFixed(2);
     return {
         currentLevel: currentLevel,
         percentage: percentage
@@ -67,6 +80,6 @@ Formulas.calculatePercentageToNextLevel = function (experience) {
 }
 
 
-if(!(typeof exports === 'undefined')) {
+if (!(typeof exports === 'undefined')) {
     module.exports = Formulas;
 }
