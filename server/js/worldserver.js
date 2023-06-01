c7c061f8c82d6bd61e77658af08629e7901c9a50
balkshamster <balkshamster@proton.me>
2023-05-31 20:34:07 -0400

    remove returning on player

--- server/js/worldserver.js
+++ server/js/worldserver.js
@@ -490,10 +490,6 @@ module.exports = World = cls.Class.extend({
     },
     
     chooseMobTarget: function(mob, hateRank) {
-        if (mob instanceof Player) {
-            this.broadcastAttacker(mob);
-            return;
-        }
         var player = this.getEntityById(mob.getHatedPlayerId(hateRank));
         
         // If the mob is not already attacking the player, create an attack link between them.
