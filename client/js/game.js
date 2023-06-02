743cda8f329ef05230eaa57516fac1184c94b2ba
wizkingprod <wizkingprod@gmail.com>
2023-06-02 10:05:15 -0500

    Adding Punk_Cyborg_378

--- client/js/game.js
+++ client/js/game.js
@@ -105,6 +105,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_aa161f880bb5cbed5813fed087358be7a44f889692e2baea3cc86c978897f0b3",
                                 "NFT_2ed54cfb0a51b0c5a66348f7efbe78b61776a4e0252901763121242888a3be44",
                                 "NFT_2bc716999d2ebea388c39b26ed6e66066d98c76412318f7f5e1c92f27f1a434a",
+                                "NFT_afcac9c2899f3c300a8e302057aa1740fda5544846a0bd026e041b75b8c50c77",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/gametypes.js
+++ client/js/gametypes.js
@@ -130,7 +130,8 @@ Types = {
         NFT_01348c02000000000000000002386f26fc1000000000000000000000000001d6: 102,
         NFT_aa161f880bb5cbed5813fed087358be7a44f889692e2baea3cc86c978897f0b3: 103,
         NFT_2ed54cfb0a51b0c5a66348f7efbe78b61776a4e0252901763121242888a3be44: 104,
-        NFT_2bc716999d2ebea388c39b26ed6e66066d98c76412318f7f5e1c92f27f1a434a: 105 // @lastid@
+        NFT_2bc716999d2ebea388c39b26ed6e66066d98c76412318f7f5e1c92f27f1a434a: 105,
+        NFT_afcac9c2899f3c300a8e302057aa1740fda5544846a0bd026e041b75b8c50c77: 106 // @lastid@
         //@nextIdLine@
     },
     
@@ -236,6 +237,7 @@ var kinds = {
     NFT_aa161f880bb5cbed5813fed087358be7a44f889692e2baea3cc86c978897f0b3: [Types.Entities.NFT_aa161f880bb5cbed5813fed087358be7a44f889692e2baea3cc86c978897f0b3, "armor"],
     NFT_2ed54cfb0a51b0c5a66348f7efbe78b61776a4e0252901763121242888a3be44: [Types.Entities.NFT_2ed54cfb0a51b0c5a66348f7efbe78b61776a4e0252901763121242888a3be44, "armor"],
     NFT_2bc716999d2ebea388c39b26ed6e66066d98c76412318f7f5e1c92f27f1a434a: [Types.Entities.NFT_2bc716999d2ebea388c39b26ed6e66066d98c76412318f7f5e1c92f27f1a434a, "armor"],
+    NFT_afcac9c2899f3c300a8e302057aa1740fda5544846a0bd026e041b75b8c50c77: [Types.Entities.NFT_afcac9c2899f3c300a8e302057aa1740fda5544846a0bd026e041b75b8c50c77, "armor"],
     // @nextSpriteLine@
 
     getType: function(kind) {
@@ -339,6 +341,8 @@ Types.getArmorRank = function(armorKind) {
         case Types.Entities.NFT_2ed54cfb0a51b0c5a66348f7efbe78b61776a4e0252901763121242888a3be44:
             return 1;
         case Types.Entities.NFT_2bc716999d2ebea388c39b26ed6e66066d98c76412318f7f5e1c92f27f1a434a:
+            return 1;
+        case Types.Entities.NFT_afcac9c2899f3c300a8e302057aa1740fda5544846a0bd026e041b75b8c50c77:
             return 1;
         // @nextarmorrank@
         default:
