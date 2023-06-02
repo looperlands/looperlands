1f3a8f895e5dd33458e19f03c50761bcbe30af63
wizkingprod <wizkingprod@gmail.com>
2023-06-02 16:41:59 -0500

    Add PC160

--- client/js/gametypes.js
+++ client/js/gametypes.js
@@ -133,7 +133,8 @@ Types = {
         NFT_2bc716999d2ebea388c39b26ed6e66066d98c76412318f7f5e1c92f27f1a434a: 105,
         NFT_afcac9c2899f3c300a8e302057aa1740fda5544846a0bd026e041b75b8c50c77: 106,
         NFT_602543f900cdec7536e01121bae04d98cf5f26dc04fdd2e65f45dcb80ccc7b52: 107,
-        NFT_8bf0ca782646556769462a2e111e63acbacba43bb50e4fff8cebd04ebfd012c4: 108 // @lastid@
+        NFT_8bf0ca782646556769462a2e111e63acbacba43bb50e4fff8cebd04ebfd012c4: 108,
+        NFT_72021bcc7f2688a31f25a0e409423c313f527a21fd06375147db912de5d25af9: 109 // @lastid@
         //@nextIdLine@
     },
     
@@ -242,6 +243,7 @@ var kinds = {
     NFT_afcac9c2899f3c300a8e302057aa1740fda5544846a0bd026e041b75b8c50c77: [Types.Entities.NFT_afcac9c2899f3c300a8e302057aa1740fda5544846a0bd026e041b75b8c50c77, "armor"],
     NFT_602543f900cdec7536e01121bae04d98cf5f26dc04fdd2e65f45dcb80ccc7b52: [Types.Entities.NFT_602543f900cdec7536e01121bae04d98cf5f26dc04fdd2e65f45dcb80ccc7b52, "armor"],
     NFT_8bf0ca782646556769462a2e111e63acbacba43bb50e4fff8cebd04ebfd012c4: [Types.Entities.NFT_8bf0ca782646556769462a2e111e63acbacba43bb50e4fff8cebd04ebfd012c4, "armor"],
+    NFT_72021bcc7f2688a31f25a0e409423c313f527a21fd06375147db912de5d25af9: [Types.Entities.NFT_72021bcc7f2688a31f25a0e409423c313f527a21fd06375147db912de5d25af9, "armor"],
     // @nextSpriteLine@
 
     getType: function(kind) {
@@ -351,6 +353,8 @@ Types.getArmorRank = function(armorKind) {
         case Types.Entities.NFT_602543f900cdec7536e01121bae04d98cf5f26dc04fdd2e65f45dcb80ccc7b52:
             return 1;
         case Types.Entities.NFT_8bf0ca782646556769462a2e111e63acbacba43bb50e4fff8cebd04ebfd012c4:
+            return 0;
+        case Types.Entities.NFT_72021bcc7f2688a31f25a0e409423c313f527a21fd06375147db912de5d25af9:
             return 0;
         // @nextarmorrank@
         default:
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -108,6 +108,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_afcac9c2899f3c300a8e302057aa1740fda5544846a0bd026e041b75b8c50c77.json',
         'text!../sprites/NFT_602543f900cdec7536e01121bae04d98cf5f26dc04fdd2e65f45dcb80ccc7b52.json',
         'text!../sprites/NFT_8bf0ca782646556769462a2e111e63acbacba43bb50e4fff8cebd04ebfd012c4.json',
+        'text!../sprites/NFT_72021bcc7f2688a31f25a0e409423c313f527a21fd06375147db912de5d25af9.json',
         // @nextSpriteImport@
     ], function() {
     
