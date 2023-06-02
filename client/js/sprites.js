a5baab93a99af44b049c0ee1023ab5f120791b3d
wizkingprod <wizkingprod@gmail.com>
2023-06-02 00:19:28 -0500

    Adding Punk_Cyborg_378 f/Database Merge & Integration

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -103,6 +103,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_fab0c923caef4d9666e41097512bb02ed1283afa0199e10031a0a24a00008daa.json',
         'text!../sprites/NFT_01348c02000000000000000002386f26fc1000000000000000000000000001d6.json',
         'text!../sprites/NFT_aa161f880bb5cbed5813fed087358be7a44f889692e2baea3cc86c978897f0b3.json',
+        'text!../sprites/NFT_afcac9c2899f3c300a8e302057aa1740fda5544846a0bd026e041b75b8c50c77.json',
         // @nextSpriteImport@
     ], function() {
     
