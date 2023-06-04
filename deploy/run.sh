264934df6728654da7726997e57c2ef264300fbc
balkshamster <balkshamster@proton.me>
2023-06-04 10:19:23 -0400

    detached

--- deploy/run.sh
+++ deploy/run.sh
@@ -6,4 +6,4 @@ docker ps  | grep looperlands | cut -d " " -f 1 | xargs docker kill
 
 # Run looperlands container
 docker run -e LOOPWORMS_API_KEY=$LOOPWORMS_API_KEY -e LOOPWORMS_LOOPERLANDS_BASE_URL=$LOOPWORMS_LOOPERLANDS_BASE_URL \
- -e DISCORD_TOKEN=$DISCORD_TOKEN -v /etc/letsencrypt/live/loopworms.io:/certs -p 8000:8000 -t looperlands
+ -e DISCORD_TOKEN=$DISCORD_TOKEN -v /etc/letsencrypt/live/loopworms.io:/certs -p 8000:8000 -t looperlands -d
