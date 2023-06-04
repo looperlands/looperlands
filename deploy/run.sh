a01442df6aadc09c50dd45cf86cda024d1846f02
balkshamster <balkshamster@proton.me>
2023-06-04 10:48:16 -0400

    move -d

--- deploy/run.sh
+++ deploy/run.sh
@@ -6,5 +6,5 @@ export $(cat /home/looperlands/.env | xargs)
 docker ps  | grep looperlands | cut -d " " -f 1 | xargs docker stop
 
 # Run looperlands container
-docker run --name looperlands-$today -e LOOPWORMS_API_KEY=$LOOPWORMS_API_KEY -e LOOPWORMS_LOOPERLANDS_BASE_URL=$LOOPWORMS_LOOPERLANDS_BASE_URL \
- -e DISCORD_TOKEN=$DISCORD_TOKEN -v /etc/letsencrypt/live/loopworms.io:/certs -p 8000:8000 -t looperlands -d
+docker run --name looperlands-$today -d -e LOOPWORMS_API_KEY=$LOOPWORMS_API_KEY -e LOOPWORMS_LOOPERLANDS_BASE_URL=$LOOPWORMS_LOOPERLANDS_BASE_URL \
+ -e DISCORD_TOKEN=$DISCORD_TOKEN -v /etc/letsencrypt/live/loopworms.io:/certs -p 8000:8000 -t looperlands 
