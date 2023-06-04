b6f6455c00696cad3247a471ed4ba9e077d350c5
balkshamster <balkshamster@proton.me>
2023-06-04 10:43:06 -0400

    add unique name

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+today=`date +"%s"`
 export $(cat /home/looperlands/.env | xargs)
 
 # Kill looperlands containers
@@ -6,4 +7,4 @@ docker ps  | grep looperlands | cut -d " " -f 1 | xargs docker stop
 
 # Run looperlands container
 docker run --name looperlands -e LOOPWORMS_API_KEY=$LOOPWORMS_API_KEY -e LOOPWORMS_LOOPERLANDS_BASE_URL=$LOOPWORMS_LOOPERLANDS_BASE_URL \
- -e DISCORD_TOKEN=$DISCORD_TOKEN -v /etc/letsencrypt/live/loopworms.io:/certs -p 8000:8000 -t looperlands -d
+ -e DISCORD_TOKEN=$DISCORD_TOKEN -v /etc/letsencrypt/live/loopworms.io:/certs -p 8000:8000 -t looperlands-$today -d
