514cb41fe3678553a9f5ab6ef2caf3dca317eda3
balkshamster <balkshamster@proton.me>
2023-06-04 11:00:59 -0400

    merge run and build into one script

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+
+today=`date +"%s"`
+export $(cat /home/looperlands/.env | xargs)
+
+# Build #
 cd looperlands
-./deploy/build.sh && ./deploy/run.sh
+git checkout .
+git pull
+./config_manager.py configs/loopworms_config.json
+docker build . -t looperlands
+
+# Deploy #
+
+# Kill looperlands containers
+docker ps  | grep looperlands | cut -d " " -f 1 | xargs docker stop
+
+# Run looperlands container
+docker run --name looperlands-$today -d -e LOOPWORMS_API_KEY=$LOOPWORMS_API_KEY -e LOOPWORMS_LOOPERLANDS_BASE_URL=$LOOPWORMS_LOOPERLANDS_BASE_URL \
+ -e DISCORD_TOKEN=$DISCORD_TOKEN -v /etc/letsencrypt/live/loopworms.io:/certs -p 8000:8000 -t looperlands 
