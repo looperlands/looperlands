e68e6d8d1be011f8a7d2eaf6122d1a8c460eb6c4
balkshamster <balkshamster@proton.me>
2023-06-04 09:47:57 -0400

    add logging

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+today=`date --iso-8601`
 export $(cat /home/looperlands/.env | xargs)
 docker run -e LOOPWORMS_API_KEY=$LOOPWORMS_API_KEY -e LOOPWORMS_LOOPERLANDS_BASE_URL=$LOOPWORMS_LOOPERLANDS_BASE_URL \
- -e DISCORD_TOKEN=$DISCORD_TOKEN -v /etc/letsencrypt/live/loopworms.io:/certs  -t -i -p 8000:8000 -t looperlands
+ -e DISCORD_TOKEN=$DISCORD_TOKEN -v /etc/letsencrypt/live/loopworms.io:/certs  -t -i -p 8000:8000 -t looperlands >> /home/looperlands/logs/$today 2>&1
