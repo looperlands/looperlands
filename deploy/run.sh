f70418c417db0511213c3633dd4c5f8f87294e53
balkshamster <balkshamster@proton.me>
2023-06-04 09:50:35 -0400

    kill existing containers

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 today=`date --iso-8601`
 export $(cat /home/looperlands/.env | xargs)
+
+# Kill looperlands containers
+docker ps  | grep looperlands | cut -d " " -f 1 | xargs docker kill
+
+# Run looperlands container
 docker run -e LOOPWORMS_API_KEY=$LOOPWORMS_API_KEY -e LOOPWORMS_LOOPERLANDS_BASE_URL=$LOOPWORMS_LOOPERLANDS_BASE_URL \
  -e DISCORD_TOKEN=$DISCORD_TOKEN -v /etc/letsencrypt/live/loopworms.io:/certs  -t -i -p 8000:8000 -t looperlands >> /home/looperlands/logs/$today 2>&1
