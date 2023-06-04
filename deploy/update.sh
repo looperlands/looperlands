4e3666ff530b13c947181abb517a04b345871b58
balkshamster <balkshamster@proton.me>
2023-06-04 10:13:16 -0400

    continue on

--- deploy/update.sh
+++ deploy/update.sh
@@ -2,4 +2,4 @@
 today=`date --iso-8601`
 logFile=/home/looperlands/logs/$today
 cd looperlands
-./deploy/build.sh && nohup ./deploy/run.sh >> $logFile 2>&1
+./deploy/build.sh && nohup ./deploy/run.sh >> $logFile 2>&1 &
