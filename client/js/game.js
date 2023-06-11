8b5939969a2410d382add0b8056d12ea0ac1b464
balkshamster <balkshamster@proton.me>
2023-06-11 17:11:33 -0400

    added rooster nft

--- client/js/game.js
+++ client/js/game.js
@@ -126,6 +126,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_2b55be5f9f0c72236a8d7e4922719f6a783d368021bac81b815737fb3b65893e",
                                 "NFT_59d24c70d2310fccf9d294bbd6c7e7368a0a098b05bb2dd981d2a97d8881aefe",
                                 "NFT_22e07e494cf1802cd2d5593df40c22cd2a2d34716e0a82d370408d5cb0072f84",
+                                "NFT_ea0b89688f49bd4e18a788e2bbfe2aaec34abe5a5c160bd98fb5389875a94fcb",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -127,6 +127,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_2b55be5f9f0c72236a8d7e4922719f6a783d368021bac81b815737fb3b65893e.json',
         'text!../sprites/NFT_59d24c70d2310fccf9d294bbd6c7e7368a0a098b05bb2dd981d2a97d8881aefe.json',
         'text!../sprites/NFT_22e07e494cf1802cd2d5593df40c22cd2a2d34716e0a82d370408d5cb0072f84.json',
+        'text!../sprites/NFT_ea0b89688f49bd4e18a788e2bbfe2aaec34abe5a5c160bd98fb5389875a94fcb.json',
         // @nextSpriteImport@
     ], function() {
     
