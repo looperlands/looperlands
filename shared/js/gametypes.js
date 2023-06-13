8361fca2383f3e28de38bec3ed2493696706905f
wizkingprod <wizkingprod@gmail.com>
2023-06-13 07:17:12 -0500

    SLB_Cart006 Integration

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -165,7 +165,8 @@ Types = {
         NFT_3a5a1e0305d3badb725085465b307366a4f15fd26eb81cfcb4574857db4d1460: 137,
         NFT_ba676e35ebaeda5cdab7765ac1e4523973a1cf12e1cce416544d2c1be26c23c2: 138,
         NFT_cee6596166b80f7a75813673dadc7508d4994c54e49b9740957233170c7d832a: 139,
-        NFT_e2c610a40bf59f9b5cb3bc646bc767e53200c25a26940d1f9c8f5e1dd159726c: 140 // @lastid@
+        NFT_e2c610a40bf59f9b5cb3bc646bc767e53200c25a26940d1f9c8f5e1dd159726c: 140,
+        NFT_ef3c4d2b065f9a59adde2424f0cb8a02f8bf77655986e2c936abbba4bac4ae78: 141 // @lastid@
         //@nextIdLine@
     },
     
@@ -307,6 +308,7 @@ var kinds = {
     NFT_ba676e35ebaeda5cdab7765ac1e4523973a1cf12e1cce416544d2c1be26c23c2: [Types.Entities.NFT_ba676e35ebaeda5cdab7765ac1e4523973a1cf12e1cce416544d2c1be26c23c2, "armor"],
     NFT_cee6596166b80f7a75813673dadc7508d4994c54e49b9740957233170c7d832a: [Types.Entities.NFT_cee6596166b80f7a75813673dadc7508d4994c54e49b9740957233170c7d832a, "armor"],
     NFT_e2c610a40bf59f9b5cb3bc646bc767e53200c25a26940d1f9c8f5e1dd159726c: [Types.Entities.NFT_e2c610a40bf59f9b5cb3bc646bc767e53200c25a26940d1f9c8f5e1dd159726c, "armor"],
+    NFT_ef3c4d2b065f9a59adde2424f0cb8a02f8bf77655986e2c936abbba4bac4ae78: [Types.Entities.NFT_ef3c4d2b065f9a59adde2424f0cb8a02f8bf77655986e2c936abbba4bac4ae78, "armor"],
     // @nextSpriteLine@
 
     
@@ -481,6 +483,8 @@ Types.getArmorRank = function(armorKind) {
         case Types.Entities.NFT_cee6596166b80f7a75813673dadc7508d4994c54e49b9740957233170c7d832a:
             return 1;
         case Types.Entities.NFT_e2c610a40bf59f9b5cb3bc646bc767e53200c25a26940d1f9c8f5e1dd159726c:
+            return 1;
+        case Types.Entities.NFT_ef3c4d2b065f9a59adde2424f0cb8a02f8bf77655986e2c936abbba4bac4ae78:
             return 1;
         // @nextarmorrank@
         default:
