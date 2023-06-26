07a824e0fa27a9a2678e4acb640bd071661b0fb8
balkshamster <128100383+balkshamster@users.noreply.github.com>
2023-06-26 17:34:05 -0400

    updated add_nfts.sh (#187)
    * updated add_nfts.sh
    
    * make the branch name a global variable
    
    * checkout main at the end
    
    * fix branch name
    
    * add parent directory
    
    * change branch name
    
    * correct variable names
    
    * added message with branch name
    
    * added new line to final message
    
    * do the push at the end of the script
    
    * correct variable names
    
    * debug message
    
    * added quotes around arguments

--- nft_template/add_nfts.sh
+++ nft_template/add_nfts.sh
@@ -14,29 +14,39 @@ fi
 
 function updateStatus() {
     nftID=$1
-    status=$2
-    curl -s -X POST -H "x-api-key: $LOOPWORMS_API_KEY" -H "Content-Type: application/json" -d "{\"nftID\": \"$nftID\", \"status\": \"$status\"}" $LOOPWORMS_LOOPERLANDS_BASE_URL/AddNewLooper/UpdateStatus.php
+    url=$LOOPWORMS_LOOPERLANDS_BASE_URL/AddNewLooper/mergedLooper.php?NFTID=$nftID
+    curl -s -X POST -H "X-Api-Key: $LOOPWORMS_API_KEY" $url
 }
+
+BRANCH_NAME=bot_add_nfts_`date +"%Y_%m_%dT%H_%M_%S"`
 
 function add_nft() {
     nftID=$1
     type=$2
     looperName=$3
-    echo $nftID $type
+    project_name=$4
+    long_nftid=$5
+    echo "Adding " $nftID $type $looperName $project_name $long_nftid
+    # Prepare the tmp directory to add the NFT spritesheet to git
     rm -rf /tmp/$nftID
     mkdir /tmp/$nftID
     cp /home/loopworms.io/public_html/DEV/LooperLands/AddNewLooper/images/1/$nftID.png /tmp/$nftID/1.png
     cp /home/loopworms.io/public_html/DEV/LooperLands/AddNewLooper/images/2/$nftID.png /tmp/$nftID/2.png
     cp /home/loopworms.io/public_html/DEV/LooperLands/AddNewLooper/images/3/$nftID.png /tmp/$nftID/3.png
     ./add_nft.sh /tmp/$nftID $nftID $type || {
-        updateStatus $nftID "error"
         echo "exiting due to add_nft.sh error"
         exit 1
     }
-    git add .
-    git commit -m "added $looperName, $nftID, $type"
-    git push
-    updateStatus $nftID "added"
+    git add ..
+    git commit -m "added $looperName, $nftID, $type, $BRANCH_NAME"
+    # add the NFT to the loopworms platform
+    /root/add_looplands_nft.sh $long_nftid $project_name $type
+    # add the picker
+    cp /home/loopworms.io/public_html/DEV/LooperLands/AddNewLooper/images/profilepic/$nftID.png /home/loopworms.io/public_html/DEV/LooperLands/img/$nftID.png
+    chown loopw4130 /home/loopworms.io/public_html/DEV/LoopQuest/img/$nftID.png
+    chgrp loopw4130 /home/loopworms.io/public_html/DEV/LoopQuest/img/$nftID.png
+    # update the status so it's not added again
+    updateStatus $nftID
     rm -rf /tmp/$nftID
 }
 
@@ -47,10 +57,16 @@ if [ "$nftsToAddJSON" = "null" ]; then
     exit 1
 fi
 
+# prepare the git repo
+git clean . -f
+git checkout .
 git checkout main
 git pull
-branchName=add_nfts_`date +"%Y_%m_%dT%H_%M_%S%z"`
-git checkout -b $branchName
-echo $nftsToAddJSON | jq -r '.[] | {short_nftid, asset_type, looper_name} | join(" ")' | while read -r line; do
-    add_nft $line
+git checkout -b $BRANCH_NAME
+
+#loop through each nft and add it
+echo $nftsToAddJSON | jq -r '.[] | {short_nftid, asset_type, looper_name, project_name, long_nftid} | join(",")' | while IFS=, read short_nftid asset_type looper_name project_name long_nftid; do
+    add_nft "$short_nftid" "$asset_type" "$looper_name" "$project_name" "$long_nftid"
 done
+
+git push --set-upstream origin $BRANCH_NAME
