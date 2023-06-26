e403793d69196f73c0b44cf0e5fac74d016efe73
loopwormsbot <balkshamster@proton.me>
2023-06-26 22:42:17 +0000

    added Robits, 0xe5c6f98ef9f2f21b851e18e55ec5251696ebda0272823e544bb4f54b17fd8e4b, armor, bot_add_nfts_2023_06_26T22_42_11

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -335,7 +335,8 @@ Types = {
         NFT_8eef5dc206ca4c66486fab9467e6cc6a518973dfab91cd0a18b5b6eee732f1e5: 306,
         NFT_9f097aa2af2242f00dfa7d08d3618f1f86faf280c236cdf776264eaedb289299: 307,
         NFT_a4f93a9ed4621bbbba5b31867021fc659a480d2a4e3e976e956f0a3a063dbdde: 308,
-        NFT_e19827fbaf9091ab2428cb4be5b03374e3871cbd493a94a6f6ed604a7d099f14: 309 // @lastid@
+        NFT_e19827fbaf9091ab2428cb4be5b03374e3871cbd493a94a6f6ed604a7d099f14: 309,
+        NFT_e5c6f98ef9f2f21b851e18e55ec5251696ebda0272823e544bb4f54b17fd8e4b: 310 // @lastid@
         //@nextIdLine@
     },
     
@@ -647,6 +648,7 @@ var kinds = {
     NFT_9f097aa2af2242f00dfa7d08d3618f1f86faf280c236cdf776264eaedb289299: [Types.Entities.NFT_9f097aa2af2242f00dfa7d08d3618f1f86faf280c236cdf776264eaedb289299, "armor"],
     NFT_a4f93a9ed4621bbbba5b31867021fc659a480d2a4e3e976e956f0a3a063dbdde: [Types.Entities.NFT_a4f93a9ed4621bbbba5b31867021fc659a480d2a4e3e976e956f0a3a063dbdde, "armor"],
     NFT_e19827fbaf9091ab2428cb4be5b03374e3871cbd493a94a6f6ed604a7d099f14: [Types.Entities.NFT_e19827fbaf9091ab2428cb4be5b03374e3871cbd493a94a6f6ed604a7d099f14, "armor"],
+    NFT_e5c6f98ef9f2f21b851e18e55ec5251696ebda0272823e544bb4f54b17fd8e4b: [Types.Entities.NFT_e5c6f98ef9f2f21b851e18e55ec5251696ebda0272823e544bb4f54b17fd8e4b, "armor"],
     // @nextSpriteLine@
 
     
@@ -1152,6 +1154,8 @@ Types.getArmorRank = function(armorKind) {
             return 1;
         case Types.Entities.NFT_e19827fbaf9091ab2428cb4be5b03374e3871cbd493a94a6f6ed604a7d099f14:
             return 0;
+        case Types.Entities.NFT_e5c6f98ef9f2f21b851e18e55ec5251696ebda0272823e544bb4f54b17fd8e4b:
+            return 1;
         // @nextarmorrank@
         default:
             console.error("Unknown armor rank");
