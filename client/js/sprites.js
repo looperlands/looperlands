6e83e117caeab397a9925d1cef9c457c70bcd404
loopwormsbot <balkshamster@proton.me>
2023-06-28 10:18:43 +0000

    added Looperbit 070, 0x947c68194040661654c45e81bda030286f3d9610cbbc039aa9833b6797925d98, armor, bot_add_nfts_2023_06_28T10_18_37

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -334,6 +334,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_75144859a01717a925c5027b007f1e46ba61a3e6e44b3cf1ff630a794e1c72cc.json',
         'text!../sprites/NFT_8842d8e11e9b08bf8200115fe03f7fcb526c6a8d1902ed631ad4d140e2e34da8.json',
         'text!../sprites/NFT_91c3748aa0f6e7ae7aad4a7d34be17230eaa78e4bf2ab54c5446639f2bdd18c7.json',
+        'text!../sprites/NFT_947c68194040661654c45e81bda030286f3d9610cbbc039aa9833b6797925d98.json',
         // @nextSpriteImport@
     ], function() {
     
