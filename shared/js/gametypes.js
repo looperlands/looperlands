a50ac27a44462529c7f9c245c8c05d8252f500b7
loopwormsbot <balkshamster@proton.me>
2023-06-28 10:18:41 +0000

    added Reese, 0x743919f3efd0e37f53d8f5f70872e8338d5acdec1c21f02b4fe385136cf907da, armor, bot_add_nfts_2023_06_28T10_18_37

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -353,7 +353,8 @@ Types = {
         NFT_35ff01ba3d7d2fb8c3a50914a779b95ca2914bba9f91fc73617248fbfe2e7569: 324,
         NFT_458996820ec9fec17041edebb43fc30bec10d44c8ef8232c361f2b1974d2d45d: 325,
         NFT_4c733a0452c22832466b7b970aa22766fbd9c064937133b222e18b11df3f31d7: 326,
-        NFT_5ecd2816304ad32aae541b723466c6681faf38a6a6cf812fdcd42da18d626d14: 327 // @lastid@
+        NFT_5ecd2816304ad32aae541b723466c6681faf38a6a6cf812fdcd42da18d626d14: 327,
+        NFT_743919f3efd0e37f53d8f5f70872e8338d5acdec1c21f02b4fe385136cf907da: 328 // @lastid@
         //@nextIdLine@
     },
     
@@ -683,6 +684,7 @@ var kinds = {
     NFT_458996820ec9fec17041edebb43fc30bec10d44c8ef8232c361f2b1974d2d45d: [Types.Entities.NFT_458996820ec9fec17041edebb43fc30bec10d44c8ef8232c361f2b1974d2d45d, "armor"],
     NFT_4c733a0452c22832466b7b970aa22766fbd9c064937133b222e18b11df3f31d7: [Types.Entities.NFT_4c733a0452c22832466b7b970aa22766fbd9c064937133b222e18b11df3f31d7, "armor"],
     NFT_5ecd2816304ad32aae541b723466c6681faf38a6a6cf812fdcd42da18d626d14: [Types.Entities.NFT_5ecd2816304ad32aae541b723466c6681faf38a6a6cf812fdcd42da18d626d14, "armor"],
+    NFT_743919f3efd0e37f53d8f5f70872e8338d5acdec1c21f02b4fe385136cf907da: [Types.Entities.NFT_743919f3efd0e37f53d8f5f70872e8338d5acdec1c21f02b4fe385136cf907da, "armor"],
     // @nextSpriteLine@
 
     
@@ -1222,6 +1224,8 @@ Types.getArmorRank = function(armorKind) {
             return 0;
         case Types.Entities.NFT_5ecd2816304ad32aae541b723466c6681faf38a6a6cf812fdcd42da18d626d14:
             return 1;
+        case Types.Entities.NFT_743919f3efd0e37f53d8f5f70872e8338d5acdec1c21f02b4fe385136cf907da:
+            return 0;
         // @nextarmorrank@
         default:
             console.error("Unknown armor rank");
