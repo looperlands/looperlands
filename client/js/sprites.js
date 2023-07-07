ddd5f552eef513d2d8d3d767c29e72c07db3e3ad
loopwormsbot <balkshamster@proton.me>
2023-07-07 11:54:32 +0000

    add Weapon 009-1, 0xcfc78dfc831f53ef319bb26e0d571f9bcb12b39fa662139feefc4d8e5ca54f4c, weapon, bot_add_nfts_2023_07_07T11_54_14

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -484,6 +484,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_b1b446a1cab1db8382745e517ed4e8c9e1ca7e58410504d7ed4fce8220f0bfd2.json',
         'text!../sprites/NFT_b35e18cf6552348982c5192e31ed52b2547afd0a62e36048f56104c64a4c083e.json',
         'text!../sprites/NFT_c8c3747e6836fde09a51142a7e06297c54c41869f40b4f48074e264371f6c600.json',
+        'text!../sprites/NFT_cfc78dfc831f53ef319bb26e0d571f9bcb12b39fa662139feefc4d8e5ca54f4c.json',
         // @nextSpriteImport@
     ], function() {
     
--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -507,7 +507,8 @@ Types = {
         NFT_ad36c96ac1b2325ff2950c2c9181fdae16f085a2e5a1709ca7e3b337b7ac6d89: 478,
         NFT_b1b446a1cab1db8382745e517ed4e8c9e1ca7e58410504d7ed4fce8220f0bfd2: 479,
         NFT_b35e18cf6552348982c5192e31ed52b2547afd0a62e36048f56104c64a4c083e: 480,
-        NFT_c8c3747e6836fde09a51142a7e06297c54c41869f40b4f48074e264371f6c600: 481 // @lastid@
+        NFT_c8c3747e6836fde09a51142a7e06297c54c41869f40b4f48074e264371f6c600: 481,
+        NFT_cfc78dfc831f53ef319bb26e0d571f9bcb12b39fa662139feefc4d8e5ca54f4c: 482 // @lastid@
         //@nextIdLine@
     },
     
@@ -991,6 +992,7 @@ var kinds = {
     NFT_b1b446a1cab1db8382745e517ed4e8c9e1ca7e58410504d7ed4fce8220f0bfd2: [Types.Entities.NFT_b1b446a1cab1db8382745e517ed4e8c9e1ca7e58410504d7ed4fce8220f0bfd2, "weapon"],
     NFT_b35e18cf6552348982c5192e31ed52b2547afd0a62e36048f56104c64a4c083e: [Types.Entities.NFT_b35e18cf6552348982c5192e31ed52b2547afd0a62e36048f56104c64a4c083e, "weapon"],
     NFT_c8c3747e6836fde09a51142a7e06297c54c41869f40b4f48074e264371f6c600: [Types.Entities.NFT_c8c3747e6836fde09a51142a7e06297c54c41869f40b4f48074e264371f6c600, "armor"],
+    NFT_cfc78dfc831f53ef319bb26e0d571f9bcb12b39fa662139feefc4d8e5ca54f4c: [Types.Entities.NFT_cfc78dfc831f53ef319bb26e0d571f9bcb12b39fa662139feefc4d8e5ca54f4c, "weapon"],
     // @nextSpriteLine@
 
     
