4aba04553f2c038012669ff588a0132448feec26
loopwormsbot <balkshamster@proton.me>
2023-07-21 10:23:06 +0000

    add Sticky Skewer 1, 0xd283412f50b526d0f664ab2c05e3e27822b3ac8e31236f7ae7f81f16165bd845, weapon, bot_add_nfts_2023_07_21T10_22_55

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -819,6 +819,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_b4b1c3d8faf9661c0a5c0f8e4e699c48130db88a7631ca832039574f0585ddc1.json',
         'text!../sprites/NFT_c0443ad1f470c3355b274de95b6275bd7bd076e1a090489e5b4560a3c3c98863.json',
         'text!../sprites/NFT_c8349036347a10820c232eee43f23ce4e3ee3c0d03d0607691374dc7c3c52dfe.json',
+        'text!../sprites/NFT_d283412f50b526d0f664ab2c05e3e27822b3ac8e31236f7ae7f81f16165bd845.json',
         // @nextSpriteImport@
     ], function() {
     
