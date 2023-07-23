9a883a4ee021ab2f1e65fb76b46e458cc5714a8b
balkshamster <balkshamster@proton.me>
2023-07-23 17:48:52 -0400

    Revert "More frequent xp changes (#281)"
    This reverts commit 03bd76d194e4631aa15fcdd8c978202f87995071.

--- server/js/worldserver.js
+++ server/js/worldserver.js
@@ -118,6 +118,7 @@ module.exports = World = cls.Class.extend({
                 //console.log(player.name + " has left the game.");
                 self.removePlayer(player);
                 self.decrementPlayerCount();
+                
                 if(self.removed_callback) {
                     self.removed_callback();
                 }
