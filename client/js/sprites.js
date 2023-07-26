d9d63873493de065b0ff2c37308bd953bbbba1dd
loopwormsbot <balkshamster@proton.me>
2023-07-26 09:31:54 +0000

    add Weaponbit 051 Karma's Bludgeoner, 0x2508fa53d70e65c0847c8d1ae75c976a1a29b5646077f0458800fa68a2b6319d, weapon, bot_add_nfts_2023_07_26T09_31_42

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -953,6 +953,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_0890bbeda4e3f15ae65f0b5f45c5c94c70887b9b85b4b459a049fd3733c3c444.json',
         'text!../sprites/NFT_17a880821fbbd01147566a04adfd912c81dec472f55ce871204e550f9ff5cd65.json',
         'text!../sprites/NFT_235721428d8d9b5ab82ef6d6eeb98aaa08f1a81ec0d45173fd2d07ce47256423.json',
+        'text!../sprites/NFT_2508fa53d70e65c0847c8d1ae75c976a1a29b5646077f0458800fa68a2b6319d.json',
         // @nextSpriteImport@
     ], function() {
     
