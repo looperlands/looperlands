a3a45d5e8ba1c1d01ec97b4afa628ef83d4e54d0
loopwormsbot <balkshamster@proton.me>
2023-07-28 09:50:30 +0000

    add PU Bounty Hunter 020, 0x6aa6fea83a8528973c4ab2f5605ad1726f47bb80a51d79b23970b8ff11bb4748, armor, bot_add_nfts_2023_07_28T09_50_21

--- client/js/game.js
+++ client/js/game.js
@@ -1037,6 +1037,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_65d52c440eae7bc223dc09ecb5fb8bc126938655c8a0563e764e37e04fdb0f72",
                                 "NFT_67a241603cdd15bf8d335e461e47410a4ae9f115e473ce086ba0024a9b0158b1",
                                 "NFT_69a78bc4daea1871aef463299634c46338e9039a8c79b5b66b3c6ba6d7c61a87",
+                                "NFT_6aa6fea83a8528973c4ab2f5605ad1726f47bb80a51d79b23970b8ff11bb4748",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1039,6 +1039,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_65d52c440eae7bc223dc09ecb5fb8bc126938655c8a0563e764e37e04fdb0f72.json',
         'text!../sprites/NFT_67a241603cdd15bf8d335e461e47410a4ae9f115e473ce086ba0024a9b0158b1.json',
         'text!../sprites/NFT_69a78bc4daea1871aef463299634c46338e9039a8c79b5b66b3c6ba6d7c61a87.json',
+        'text!../sprites/NFT_6aa6fea83a8528973c4ab2f5605ad1726f47bb80a51d79b23970b8ff11bb4748.json',
         // @nextSpriteImport@
     ], function() {
     
