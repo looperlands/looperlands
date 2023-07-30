d9fdba4abc7a34c57b20cf4644384ba78f5a3436
loopwormsbot <balkshamster@proton.me>
2023-07-30 12:40:47 +0000

    add Weaponbit 050 (Gingy's Lollipop), 0x4c2c237524e86f106d2d611738458b5ce874a459e548b73d1dd09ef27f5c2494, weapon, bot_add_nfts_2023_07_30T12_40_42

--- client/js/game.js
+++ client/js/game.js
@@ -1066,6 +1066,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_fea28fb79727b6043252b0c40719d891c55a574bfaca30d6bb1442b19fdff7b7",
                                 "NFT_3268e2ff549abf4275fdee618fc673774ff23ca6b826bd04ace8b7bc51d476c5",
                                 "NFT_3c996fc744055f6354d8d44820e17101379276a2570382186ff4875fc7b69bc0",
+                                "NFT_4c2c237524e86f106d2d611738458b5ce874a459e548b73d1dd09ef27f5c2494",
                                 // @nextSpriteLine@
                             ];
         },
