17898a69dc2299f400eb60224875ce978b7f1799
balkshamster <balkshamster@proton.me>
2023-08-04 17:10:57 -0400

    getting closer to copying

---
+++ copymap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+CONTAINER_ID=$1
+LOOPERLANDS_GIT_REPO=$2
+if [ -z "$CONTAINER_ID" ]
+  then
+    echo "Missing CONTAINER_ID argument"
+    exit 1
+fi
+
+if [ -z "$LOOPERLANDS_GIT_REPO" ]
+  then
+    echo "Missing LOOPERLANDS_GIT_REPO argument"
+    exit 1
+fi
+
+echo $CONTAINER_ID, $LOOPERLANDS_GIT_REPO
+docker cp $CONTAINER_ID:/opt/app/ $LOOPERLANDS_GIT_REPO
