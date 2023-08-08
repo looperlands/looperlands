ffa2640cf642d1a04fc312aca36de0a1137b0028
loopwormsbot <balkshamster@proton.me>
2023-08-08 11:41:09 +0000

    add Spirit Blade, 0xffde10408a493b78f9cd5c4fe93234e298d58dada309645a94fbbaca74af4c29, weapon, bot_add_nfts_2023_08_08T11_40_54

--- client/js/game.js
+++ client/js/game.js
@@ -1224,6 +1224,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_e84e79ee479e3ff3064b8a033fbd90c1be78e30ef64321e4622e1293dd348ce0",
                                 "NFT_f72ea587df5dd53bcc888dbc59a2b2b2a6bfa27de819be6e68e4a31488486543",
                                 "NFT_fb1a38d1124713a2bda48676a7b9b4797db85258f7ca815ed94782f264cf7c1a",
+                                "NFT_ffde10408a493b78f9cd5c4fe93234e298d58dada309645a94fbbaca74af4c29",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1229,6 +1229,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_e84e79ee479e3ff3064b8a033fbd90c1be78e30ef64321e4622e1293dd348ce0.json',
         'text!../sprites/NFT_f72ea587df5dd53bcc888dbc59a2b2b2a6bfa27de819be6e68e4a31488486543.json',
         'text!../sprites/NFT_fb1a38d1124713a2bda48676a7b9b4797db85258f7ca815ed94782f264cf7c1a.json',
+        'text!../sprites/NFT_ffde10408a493b78f9cd5c4fe93234e298d58dada309645a94fbbaca74af4c29.json',
         // @nextSpriteImport@
     ], function() {
     
