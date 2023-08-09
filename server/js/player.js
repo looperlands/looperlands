e2bbcf9c5ec3a1c8383e14e6d9986a93a191c3c6
balkshamster <128100383+balkshamster@users.noreply.github.com>
2023-08-09 07:17:32 -0400

    Save map id to avatar (#361)
    * api key save not working
    
    * remove logs, and post => get
    
    * remove CDN

--- server/js/player.js
+++ server/js/player.js
@@ -106,7 +106,7 @@ module.exports = Player = Character.extend({
                 self.hasEnteredGame = true;
                 self.isDead = false;
                 discord.sendMessage(`Player ${self.name} joined the game.`);
-                
+                dao.saveAvatarMapId(playerCache.nftId, playerCache.mapId);
             }
             else if(action === Types.Messages.WHO) {
                 message.shift();
