d2b9c7eab3de4075a5b04b02afae6597b9d9dd3b
loopwormsbot <balkshamster@proton.me>
2023-08-10 12:13:59 +0000

    add PixelTar 3181, 0x01348c00000000000000000002386f26fc1000000000000000000000000000b5, armor, bot_add_nfts_2023_08_10T12_13_54

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1248,6 +1248,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_c0b024c7588851125e05ffad4d1f3108502215415ba4f5f2f94146886aa99d4e.json',
         'text!../sprites/NFT_e9d858a6988b291f78f7c9641b694827765f4f01eb9b95f2b2ca63423ae12ac9.json',
         'text!../sprites/NFT_f393cc6954db2b0594f3fcb8f75faa8b4afa59e03b5e09b829c3078e33c61177.json',
+        'text!../sprites/NFT_01348c00000000000000000002386f26fc1000000000000000000000000000b5.json',
         // @nextSpriteImport@
     ], function() {
     
