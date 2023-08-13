a43779c3b57f57065b5f3539916be744304a2a34
loopwormsbot <balkshamster@proton.me>
2023-08-13 13:18:43 +0000

    add Short Destroyer 29, 0x88656755a8a69ded24d2256b2c5b618f2fcdf1da56e3c79bb310630233bfba89, armor, bot_add_nfts_2023_08_13T13_18_34

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1295,6 +1295,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_6f767cb9204c42c690b5aa4517b305a442de416b75bcb624b809c5444f81be2f.json',
         'text!../sprites/NFT_8221e414054faf45849b1507042580f970f5f796c6c7bd77ffcb75bb52360d78.json',
         'text!../sprites/NFT_85e9531473cff75d0abed53a918f1d30b5c5a6e5633c705638a97f97a0dd1739.json',
+        'text!../sprites/NFT_88656755a8a69ded24d2256b2c5b618f2fcdf1da56e3c79bb310630233bfba89.json',
         // @nextSpriteImport@
     ], function() {
     
