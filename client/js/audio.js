6598c29cb5a2642579bc92a5fb09920d23d0f79a
ordinaryadam <joshiadam@hotmail.com>
2023-08-12 20:13:45 -0700

    upd

--- client/js/audio.js
+++ client/js/audio.js
@@ -11,7 +11,7 @@ define(['area'], function(Area) {
             this.game = game;
             this.currentMusic = null;
             this.areas = [];
-            this.musicNames = ["village","beach", "forest", "cave", "desert", "lavaland", "boss", "oavillage", "oaforest", "overworld","oacave","oawind","oaforesttown","oatitle","oaporttown"];
+            this.musicNames = ["village","beach", "forest", "cave", "desert", "lavaland", "boss", "oavillage", "oaforest", "overworld","oacave","oawind","oaforesttown","oatitle","oaporttown","oaplanes"];
             this.soundNames = ["loot", "hit1", "hit2", "hurt", "heal", "chat", "revive", "death", "firefox", "achievement", "kill1", "kill2", "noloot", "teleport", "chest", "npc", "npc-end"];
             
             var loadSoundFiles = function() {
