b3d9ad2ff379f8943a2ea74758057ea51162fe33
balkshamster <balkshamster@proton.me>
2023-08-20 19:56:44 -0400

    modify new line

--- entity_template/add_object.sh
+++ entity_template/add_object.sh
@@ -45,7 +45,7 @@ newLine="                                \"$OBJECT_ID\",\n                      
 sed -e "s#.*@nextObjectLine@.*#$newLine#g" ../client/js/game.js > tmp2.js
 mv tmp2.js ../client/js/game.js
 
+newLine=`sed -e "s/ID/$OBJECT_ID/g" ./item-template.js` 
 
-newLine="                                \"$OBJECT_ID\",\n                                // @nextItemLine@"
-sed -e "s#.*@nextItemLine@.*#$newLine#g" ../client/js/game.js > tmp2.js
+sed -e "s#.*@nextItemLine@.*#$newLine#g" ../client/js/items.js > tmp2.js
 mv tmp2.js ../client/js/items.js
