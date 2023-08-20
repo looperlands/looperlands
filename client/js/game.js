645551dc32aed322e1cf9528710d663466828cfa
balkshamster <128100383+balkshamster@users.noreply.github.com>
2023-08-20 06:45:15 -0400

    Bot add nfts 2023 08 20 t10 44 45 (#419)
    * add Bee-ndiana Jones by Quickosss, 0x0befee4d47c0090ff4140c13766d611083ae13f722624c0d5e3059bff1c999c0, armor, bot_add_nfts_2023_08_20T10_44_45
    
    * add B for BEEWHOLE by DiamondApe, 0x4973970cacf01f878fffeb983e5693ec69cdd9148e727a6c1b43db7f5b84fecf, armor, bot_add_nfts_2023_08_20T10_44_45
    
    * add Pixel Mushie 11, 0xfd166db63b60fcfa312f336a0f614f0b9683f24bee3bdc84276e4a0203d64be5, armor, bot_add_nfts_2023_08_20T10_44_45

--- client/js/game.js
+++ client/js/game.js
@@ -1377,6 +1377,9 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_5b174c0a0251c5d49b5de0f898f18453c7d1d34778c42c351fd5d79a96745149",
                                 "NFT_cda20dd1a13999f713508d97778bf2b4697e9b2bc47f9ab25f9eb01f55dcafc2",
                                 "NFT_f14342d440ee7bdc61f9d901614b8e8b0a0e4f714af1132648d8bd0b2997d609",
+                                "NFT_0befee4d47c0090ff4140c13766d611083ae13f722624c0d5e3059bff1c999c0",
+                                "NFT_4973970cacf01f878fffeb983e5693ec69cdd9148e727a6c1b43db7f5b84fecf",
+                                "NFT_fd166db63b60fcfa312f336a0f614f0b9683f24bee3bdc84276e4a0203d64be5",
                                 // @nextSpriteLine@
                             ];                          
         },
