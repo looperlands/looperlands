07aa8d94a5705a60010fb9c8ed8c62b5506a78d9
balkshamster <balkshamster@proton.me>
2023-08-21 08:47:42 -0400

    add tabs

--- entity_template/item-template.js
+++ entity_template/item-template.js
@@ -1,1 +1,1 @@
-ID:Item.extend({init:function(id){this._super(id,Types.Entities.ID,"object");this.lootMessage="";}}),\n//@nextItemLine@
+        ID:Item.extend({init:function(id){this._super(id,Types.Entities.ID,"object");this.lootMessage="";}}),\n        // @nextItemLine@
