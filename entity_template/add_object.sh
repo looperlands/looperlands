fe2038dd1d44dec0855be5f1f25583d038063d66
balkshamster <balkshamster@proton.me>
2023-08-22 13:21:46 -0400

    adding item-

--- entity_template/add_object.sh
+++ entity_template/add_object.sh
@@ -52,7 +52,7 @@ newLine="        'text!../sprites/item-$OBJECT_ID.json',\n        // @nextObject
 sed -e "s#.*@nextObjectImport@.*#$newLine#g" ../client/js/sprites.js > tmp.js
 mv tmp.js ../client/js/sprites.js
 
-newLine="                                \"$OBJECT_ID\",\n                                // @nextObjectLine@"
+newLine="                                \"item-$OBJECT_ID\",\n                                // @nextObjectLine@"
 sed -e "s#.*@nextObjectLine@.*#$newLine#g" ../client/js/game.js > tmp2.js
 mv tmp2.js ../client/js/game.js
 
