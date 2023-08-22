54e10c093bab5e7a801f188a1d26a5253b56a261
loopwormsbot <balkshamster@proton.me>
2023-08-22 11:16:04 +0000

    add SLB Cart 036, 0xe35d91ee21c1110447790dde046abe498c74172ba6f87905001d28bda5441cf4, armor, bot_add_nfts_2023_08_22T11_15_55

--- client/js/game.js
+++ client/js/game.js
@@ -1402,6 +1402,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_b6073959093bc46e11542069efbe134978061f534a3c524b1b89041478bf99ea",
                                 "NFT_c0ebcde38a48651f1125c376f51094c6bcfef7494f5da0b6f7d9829b0e0df4dc",
                                 "NFT_cab59f1e0e4728b987342a5c27b7cffecc7e06c235c9df1e825bda399a233d87",
+                                "NFT_e35d91ee21c1110447790dde046abe498c74172ba6f87905001d28bda5441cf4",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1418,6 +1418,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_b6073959093bc46e11542069efbe134978061f534a3c524b1b89041478bf99ea.json',
         'text!../sprites/NFT_c0ebcde38a48651f1125c376f51094c6bcfef7494f5da0b6f7d9829b0e0df4dc.json',
         'text!../sprites/NFT_cab59f1e0e4728b987342a5c27b7cffecc7e06c235c9df1e825bda399a233d87.json',
+        'text!../sprites/NFT_e35d91ee21c1110447790dde046abe498c74172ba6f87905001d28bda5441cf4.json',
         // @nextSpriteImport@
     ], function() {
     
