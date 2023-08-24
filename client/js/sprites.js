a3efa79d86b5694434a1b7d3bcf577447322e722
loopwormsbot <balkshamster@proton.me>
2023-08-24 10:21:12 +0000

    add Voidshiv 2, 0x229e065ad1a2261f837b42ef797a113b449988f30e07f0ca0d61e03fce146fcc, weapon, bot_add_nfts_2023_08_24T10_21_08

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1434,6 +1434,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_ea879b7be3e3876dda4ceab3031718904fb27d2463df153ceb83dcb7578278f3.json',
         'text!../sprites/NFT_01348c01000000000000000002386f26fc1000000000000000000000000003d3.json',
         'text!../sprites/NFT_222d56c3ef463b685a6c2fa413ee8ef7e010f66d935a97668a002d644c794b5b.json',
+        'text!../sprites/NFT_229e065ad1a2261f837b42ef797a113b449988f30e07f0ca0d61e03fce146fcc.json',
         // @nextSpriteImport@
     ], function() {
     
