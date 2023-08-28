f6b1222a7566663557957de39072e66769eecead
ordinaryadam <112909750+ordinaryadam@users.noreply.github.com>
2023-08-28 04:39:04 -0700

    Oa map 08 27 2023 (#443)
    * upd
    
    * upd
    
    * upd
    
    * upd

--- server/js/properties.js
+++ server/js/properties.js
@@ -303,7 +303,29 @@ var Properties = {
         hp: 300,
         armor: 3,
         weapon: 3,
+    },
+
+    alaric: {
+        hp: 10,
+        armor: 1,
+        weapon: 1,
+        friendly: true
+    },
+
+    jayce: {
+        hp: 10,
+        armor: 1,
+        weapon: 1,
+        friendly: true
+    },
+    orlan: {
+        hp: 10,
+        armor: 1,
+        weapon: 1,
+        friendly: true
     }
+
+
 };
 
 Properties.getArmorLevel = function(kind) {
