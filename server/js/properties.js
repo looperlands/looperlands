48ea077c256ec52871965ff1cacde187d5cfbabd
ordinaryadam <112909750+ordinaryadam@users.noreply.github.com>
2023-08-31 04:51:11 -0700

    upd (#454)

--- server/js/properties.js
+++ server/js/properties.js
@@ -136,7 +136,7 @@ var Properties = {
     slime: {
         level: 1,
         drops: {
-            potion: 50,
+            potion: 95,
         },
     },
 
@@ -189,7 +189,7 @@ var Properties = {
     redslime: {
         level: 2,
         drops: {
-            potion: 100,
+            potion: 90,
         },
     },     
 
