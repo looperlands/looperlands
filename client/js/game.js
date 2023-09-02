f3aa29a8c4ad5170db1f44dd0bd9eb5534b7aad0
loopwormsbot <balkshamster@proton.me>
2023-09-02 10:01:30 +0000

    add LOOPatello, 0x782a0dd46ab88eec2046233a11d837c57454375f033192fd067d486159bdb654, armor, bot_add_nfts_2023_09_02T10_01_21

--- client/js/game.js
+++ client/js/game.js
@@ -1526,6 +1526,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_6bd9ce59536d51427d3c9d5f45245647685886042823b87f4498269e558e5fbe",
                                 "NFT_6cce90e4115b0e11439e6ef80036d700a3fb3ba96a3b7b3508cb2e0a355ded56",
                                 "NFT_726ea26ee7029afedd8f24ba529c413d9350e605205593f64007b72d4541e7ab",
+                                "NFT_782a0dd46ab88eec2046233a11d837c57454375f033192fd067d486159bdb654",
                                 // @nextSpriteLine@
                             ];                          
         },
