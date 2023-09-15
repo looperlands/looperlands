84f5e99fafb5bd9a5478ec9d92e87184c863f74c
loopwormsbot <balkshamster@proton.me>
2023-09-15 10:02:10 +0000

    add Stupid LooperMonkey 002 - Purple, 0x6cda8830749a35abefd55c821ad06117fd427e7f8969fbb7ad38a4235edaea5c, armor, bot_add_nfts_2023_09_15T10_02_05

--- client/js/game.js
+++ client/js/game.js
@@ -1710,6 +1710,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_01e883e031f55f7232e2543d8a1c9a352ad14536c1eab0dde4c6f6ec4ac64309",
                                 "NFT_2a4b9aec338320cfba56450d6741e522542647aa067f07dd1525ed811cc80676",
                                 "NFT_35563f0fd195ce001c638eee6964860d0a163e5326841462f61e1b7b404688c0",
+                                "NFT_6cda8830749a35abefd55c821ad06117fd427e7f8969fbb7ad38a4235edaea5c",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1825,6 +1825,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_01e883e031f55f7232e2543d8a1c9a352ad14536c1eab0dde4c6f6ec4ac64309.json',
         'text!../sprites/NFT_2a4b9aec338320cfba56450d6741e522542647aa067f07dd1525ed811cc80676.json',
         'text!../sprites/NFT_35563f0fd195ce001c638eee6964860d0a163e5326841462f61e1b7b404688c0.json',
+        'text!../sprites/NFT_6cda8830749a35abefd55c821ad06117fd427e7f8969fbb7ad38a4235edaea5c.json',
         // @nextSpriteImport@
     ], function() {
     
