1667e1dceeb283387513071dfb0a17c7c703c6d9
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:56:14 +0000

    add Emerald Leviathan, 0x62cd76c930b1d0815bf16e6514ff5dae2b9def97ff494a7b03ce6b47ddc7649c, weapon, bot_add_nfts_2023_09_15T22_54_24

--- client/js/game.js
+++ client/js/game.js
@@ -1836,6 +1836,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_6216713721e0a4845c7ad60539578f8576cbfd55cb569ff77b8b90260b2e6d69",
                                 "NFT_6230ebb6e3e4687fc88b4b333e8a4017ae76b0d197931a376060024bd1843bdc",
                                 "NFT_6268294e620c03ec9084a5bf5abe2e3ad07a795e50836a88ba320eec135b8009",
+                                "NFT_62cd76c930b1d0815bf16e6514ff5dae2b9def97ff494a7b03ce6b47ddc7649c",
                                 // @nextSpriteLine@
                             ];                          
         },
