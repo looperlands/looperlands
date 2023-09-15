48715b3c0f491cd7307d879bbabc9ecd43b95613
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:56:56 +0000

    add Darius Silverblade, 0x7c71ee7666c23bedebda30b8830be559939df0d3afec4e280e1dc4e2e1245836, armor, bot_add_nfts_2023_09_15T22_54_24

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1998,6 +1998,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_7ad2d21dc7b63554e92afabe010e45ff50d0c584ea9a8bdb5695217ce4acf91e.json',
         'text!../sprites/NFT_7ae623a460e649368b695c832d71235a960c45b3bfc664ba8aae17f6b2b41490.json',
         'text!../sprites/NFT_7c2165364cc9d74b6bea85d2d68a396efb1b9298f665d8aea214d72589eb49f3.json',
+        'text!../sprites/NFT_7c71ee7666c23bedebda30b8830be559939df0d3afec4e280e1dc4e2e1245836.json',
         // @nextSpriteImport@
     ], function() {
     
