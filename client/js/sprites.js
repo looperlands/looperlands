e6f9b21c4d537c64e65eb374a6197ccaa08bf1cb
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:56:39 +0000

    add Leon Thundergale, 0x70f10ff92eae50eafb6ed68584a76ec910e9632ab7ec5450fb44f492f509c2ac, armor, bot_add_nfts_2023_09_15T22_54_24

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1980,6 +1980,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_7086ee2a0599fff9cffd63280bcb85fb6d2dfb8aaaf1dfaff6ebae742f68cb66.json',
         'text!../sprites/NFT_70ba9c6f3c92041b50d3a91eecf1b9f9d8b6bf35c2fb7d3f17b4be8dc5355ff3.json',
         'text!../sprites/NFT_70e6e39fcb630ef1ccffc123ecd0eb24b0743ef945884e4ec8aa6e09b384021d.json',
+        'text!../sprites/NFT_70f10ff92eae50eafb6ed68584a76ec910e9632ab7ec5450fb44f492f509c2ac.json',
         // @nextSpriteImport@
     ], function() {
     
