63c03bb51805171b3ab95fb47256beebc9e1d03b
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:59:37 +0000

    add Crimson Magestaff, 0xe9da33926888570fc6b23a953469d1df61d00b448e75e815008fc0084b75cb7a, weapon, bot_add_nfts_2023_09_15T22_54_24

--- client/js/game.js
+++ client/js/game.js
@@ -2055,6 +2055,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_e86bf3d8debfed9d342e7892bb98df65032ee5d1acff8e3c552c2094a2af00eb",
                                 "NFT_e8c26b2fd84e5402474205d36bf41e3963402bd206da7c4950698326f0f331c8",
                                 "NFT_e8c71565d80ff1d3158e7c62ed60fd9b304283eef7509fe6acd87036c41a1340",
+                                "NFT_e9da33926888570fc6b23a953469d1df61d00b448e75e815008fc0084b75cb7a",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2172,6 +2172,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_e86bf3d8debfed9d342e7892bb98df65032ee5d1acff8e3c552c2094a2af00eb.json',
         'text!../sprites/NFT_e8c26b2fd84e5402474205d36bf41e3963402bd206da7c4950698326f0f331c8.json',
         'text!../sprites/NFT_e8c71565d80ff1d3158e7c62ed60fd9b304283eef7509fe6acd87036c41a1340.json',
+        'text!../sprites/NFT_e9da33926888570fc6b23a953469d1df61d00b448e75e815008fc0084b75cb7a.json',
         // @nextSpriteImport@
     ], function() {
     
