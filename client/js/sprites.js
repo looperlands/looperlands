f0223fcf1813b3f46ad25f47f4239bb4366481a0
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:59:45 +0000

    add Ignatius Ironhand, 0xed990a68f79181af51efbbfc6e6990518c47bb79907f8d6a6c5e879e9d0636b2, armor, bot_add_nfts_2023_09_15T22_54_24

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2180,6 +2180,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_ebbc32f9982f16e4b8efe975f5b9bc66958cb1549c6ddd57e61086a56aa29ca1.json',
         'text!../sprites/NFT_ed2b9c5fb5c5699f043aa8e1ad4f35c438f94b13e3ab1fe197bd612360c1818c.json',
         'text!../sprites/NFT_ed8640812e1ea21bfef315f4d51d334b6f7c4e99a8bebcb18e9f8cb8d2c9de32.json',
+        'text!../sprites/NFT_ed990a68f79181af51efbbfc6e6990518c47bb79907f8d6a6c5e879e9d0636b2.json',
         // @nextSpriteImport@
     ], function() {
     
