c9800f7348b0be44ebe4ec75cb0b52401f97583b
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:57:28 +0000

    add Sunforged Spear, 0x8ec5e5a1d770fa64c6d739db4af7dee463f953a054cb1cb55e492b0d63974efb, weapon, bot_add_nfts_2023_09_15T22_54_24

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2033,6 +2033,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_8e525b0a01e81feef8c4a14fb411884b117d5192cdbab2d7ebb1c5cef698dc1e.json',
         'text!../sprites/NFT_8eb02e0e25a99460c0f8ae9d7b2343d4db41ef114d582071dd3272068e800603.json',
         'text!../sprites/NFT_8eb9a10b79c9ff69447f10e88a7cd095c77e8b9231f4d9c95065b8e305932e04.json',
+        'text!../sprites/NFT_8ec5e5a1d770fa64c6d739db4af7dee463f953a054cb1cb55e492b0d63974efb.json',
         // @nextSpriteImport@
     ], function() {
     
