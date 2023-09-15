53083522bf07d25625aa10f6475ddfefd4c604a7
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:57:18 +0000

    add Edgar Brightmace, 0x899f8a0471722c67167f5709fab469e48fa05cd839401bf06397c4fc7dba129b, armor, bot_add_nfts_2023_09_15T22_54_24

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -2050,7 +2050,8 @@ Types = {
         NFT_89233f3366f15b841deae4488dffa4bee6bb6c7dbb94f7a5a3db4389bfc88156: 1863,
         NFT_8963932f98bdcc50886ff42a444e5ad5e2bf820838254ba9c1518c9a1bf174cb: 1864,
         NFT_8966ff96e6197d7aece6501eb63abe6146f86f17dd7201172a6fe983502ea59c: 1865,
-        NFT_896d6eb410a1451ca7d0712798fcf3de8426b378a912eaaf23ba5ba680d95c4f: 1866 // @lastid@
+        NFT_896d6eb410a1451ca7d0712798fcf3de8426b378a912eaaf23ba5ba680d95c4f: 1866,
+        NFT_899f8a0471722c67167f5709fab469e48fa05cd839401bf06397c4fc7dba129b: 1867 // @lastid@
         //@nextIdLine@
     },
     
@@ -4097,6 +4098,7 @@ var kinds = {
     NFT_8963932f98bdcc50886ff42a444e5ad5e2bf820838254ba9c1518c9a1bf174cb: [Types.Entities.NFT_8963932f98bdcc50886ff42a444e5ad5e2bf820838254ba9c1518c9a1bf174cb, "armor"],
     NFT_8966ff96e6197d7aece6501eb63abe6146f86f17dd7201172a6fe983502ea59c: [Types.Entities.NFT_8966ff96e6197d7aece6501eb63abe6146f86f17dd7201172a6fe983502ea59c, "weapon"],
     NFT_896d6eb410a1451ca7d0712798fcf3de8426b378a912eaaf23ba5ba680d95c4f: [Types.Entities.NFT_896d6eb410a1451ca7d0712798fcf3de8426b378a912eaaf23ba5ba680d95c4f, "weapon"],
+    NFT_899f8a0471722c67167f5709fab469e48fa05cd839401bf06397c4fc7dba129b: [Types.Entities.NFT_899f8a0471722c67167f5709fab469e48fa05cd839401bf06397c4fc7dba129b, "armor"],
     // @nextSpriteLine@
 
     
