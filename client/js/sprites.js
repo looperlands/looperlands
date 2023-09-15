f9da2b43f52fa9f104ca565385fd6e7123988c59
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:56:11 +0000

    add Bastard Sword, 0x61d365dd7a828beab71db6a8e5e292eb7ea347df831bd3e66e7c5abe66416fc7, weapon, bot_add_nfts_2023_09_15T22_54_24

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1948,6 +1948,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_61346c17f3232ee6321d03530ec434d77cc797be64b781d798ef33974ddba03f.json',
         'text!../sprites/NFT_614f187631b1827d105a4c06237dae3e0c4c8ba42d92d3179a0fb4d9e16b1efc.json',
         'text!../sprites/NFT_616a1cdcc8c7bc2192034715b6fdf71ee939e4b1c225ca9713d73aade44dd034.json',
+        'text!../sprites/NFT_61d365dd7a828beab71db6a8e5e292eb7ea347df831bd3e66e7c5abe66416fc7.json',
         // @nextSpriteImport@
     ], function() {
     
