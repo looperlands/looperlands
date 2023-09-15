a1374535b033464027fb4d99dcb19cd12b7adf62
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:59:09 +0000

    add Divine Reckoning, 0xdbe8cfa1f75a18c4b064a51a1a8ea148ddf0a73a053cb36d9be9d0b9c95436b2, weapon, bot_add_nfts_2023_09_15T22_54_24

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -2172,7 +2172,8 @@ Types = {
         NFT_da9af902bba64c880e3c843873a35b3de20413b2c27f752e36e2616ac9288cac: 1985,
         NFT_dab7e2b47212677d9746717f399a0f3e1fc2c8abf4ad4fca509e6a12696b031a: 1986,
         NFT_daed3c471c87b2ac546fb61d5e1faf0b92b761556aa08005fa750fcc00862095: 1987,
-        NFT_db9a70c104136f43ddc236fc7bb0b5fed62c8c00108e167b5da115a0583de0cd: 1988 // @lastid@
+        NFT_db9a70c104136f43ddc236fc7bb0b5fed62c8c00108e167b5da115a0583de0cd: 1988,
+        NFT_dbe8cfa1f75a18c4b064a51a1a8ea148ddf0a73a053cb36d9be9d0b9c95436b2: 1989 // @lastid@
         //@nextIdLine@
     },
     
@@ -4341,6 +4342,7 @@ var kinds = {
     NFT_dab7e2b47212677d9746717f399a0f3e1fc2c8abf4ad4fca509e6a12696b031a: [Types.Entities.NFT_dab7e2b47212677d9746717f399a0f3e1fc2c8abf4ad4fca509e6a12696b031a, "weapon"],
     NFT_daed3c471c87b2ac546fb61d5e1faf0b92b761556aa08005fa750fcc00862095: [Types.Entities.NFT_daed3c471c87b2ac546fb61d5e1faf0b92b761556aa08005fa750fcc00862095, "armor"],
     NFT_db9a70c104136f43ddc236fc7bb0b5fed62c8c00108e167b5da115a0583de0cd: [Types.Entities.NFT_db9a70c104136f43ddc236fc7bb0b5fed62c8c00108e167b5da115a0583de0cd, "weapon"],
+    NFT_dbe8cfa1f75a18c4b064a51a1a8ea148ddf0a73a053cb36d9be9d0b9c95436b2: [Types.Entities.NFT_dbe8cfa1f75a18c4b064a51a1a8ea148ddf0a73a053cb36d9be9d0b9c95436b2, "weapon"],
     // @nextSpriteLine@
 
     
