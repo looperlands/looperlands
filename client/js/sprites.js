5c1fd2eaec31f57e0de6e8202a9d117394a133ed
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:57:36 +0000

    add Frost Eclipse Blade, 0x986fe91b325904d6f8f4405a7e0d79f49aa357d6e345cd82e5de959277b409d7, weapon, bot_add_nfts_2023_09_15T22_54_24

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2041,6 +2041,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_948cc367cc0bc338332caac2b861ac3fc5010f791ab940bd9f54c9902eb41357.json',
         'text!../sprites/NFT_94ed94ebfe9dac19e64a1327fdc1e9ada07aa984179ae1612a7f8b840ee9d155.json',
         'text!../sprites/NFT_982ee115410c312df81e28a61669d6eacc9b54d62d29af47c074370aa4b5d961.json',
+        'text!../sprites/NFT_986fe91b325904d6f8f4405a7e0d79f49aa357d6e345cd82e5de959277b409d7.json',
         // @nextSpriteImport@
     ], function() {
     
