ad86a990b22f5243784b8911927d116e01abdaf1
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:59:44 +0000

    add Steel Exile Sword, 0xed8640812e1ea21bfef315f4d51d334b6f7c4e99a8bebcb18e9f8cb8d2c9de32, weapon, bot_add_nfts_2023_09_15T22_54_24

--- client/js/game.js
+++ client/js/game.js
@@ -2062,6 +2062,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_eba35a8e9816be2bb98558a66b854079a518325e13e0b99bfb3b309228790f77",
                                 "NFT_ebbc32f9982f16e4b8efe975f5b9bc66958cb1549c6ddd57e61086a56aa29ca1",
                                 "NFT_ed2b9c5fb5c5699f043aa8e1ad4f35c438f94b13e3ab1fe197bd612360c1818c",
+                                "NFT_ed8640812e1ea21bfef315f4d51d334b6f7c4e99a8bebcb18e9f8cb8d2c9de32",
                                 // @nextSpriteLine@
                             ];                          
         },
