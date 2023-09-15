934247c783e76380c8fbb77b0d86313b4c04ea77
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:59:40 +0000

    add Amethyst Netherhammer, 0xeba35a8e9816be2bb98558a66b854079a518325e13e0b99bfb3b309228790f77, weapon, bot_add_nfts_2023_09_15T22_54_24

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -2204,7 +2204,8 @@ Types = {
         NFT_e9da33926888570fc6b23a953469d1df61d00b448e75e815008fc0084b75cb7a: 2017,
         NFT_e9f90cef581f4d5cb7d691ed68b2043a00d9f0d832b6260011f22cb14707d46f: 2018,
         NFT_eb1f197e3228e7467ad751575a617c9e2f1a3e2e5e21ae2ffd28430dc2c2a2e1: 2019,
-        NFT_eb8f92c36e7d07e52d88c5a141474df78869f2b8ece3eab1cbeecd331ee7f47b: 2020 // @lastid@
+        NFT_eb8f92c36e7d07e52d88c5a141474df78869f2b8ece3eab1cbeecd331ee7f47b: 2020,
+        NFT_eba35a8e9816be2bb98558a66b854079a518325e13e0b99bfb3b309228790f77: 2021 // @lastid@
         //@nextIdLine@
     },
     
@@ -4405,6 +4406,7 @@ var kinds = {
     NFT_e9f90cef581f4d5cb7d691ed68b2043a00d9f0d832b6260011f22cb14707d46f: [Types.Entities.NFT_e9f90cef581f4d5cb7d691ed68b2043a00d9f0d832b6260011f22cb14707d46f, "weapon"],
     NFT_eb1f197e3228e7467ad751575a617c9e2f1a3e2e5e21ae2ffd28430dc2c2a2e1: [Types.Entities.NFT_eb1f197e3228e7467ad751575a617c9e2f1a3e2e5e21ae2ffd28430dc2c2a2e1, "armor"],
     NFT_eb8f92c36e7d07e52d88c5a141474df78869f2b8ece3eab1cbeecd331ee7f47b: [Types.Entities.NFT_eb8f92c36e7d07e52d88c5a141474df78869f2b8ece3eab1cbeecd331ee7f47b, "weapon"],
+    NFT_eba35a8e9816be2bb98558a66b854079a518325e13e0b99bfb3b309228790f77: [Types.Entities.NFT_eba35a8e9816be2bb98558a66b854079a518325e13e0b99bfb3b309228790f77, "weapon"],
     // @nextSpriteLine@
 
     
