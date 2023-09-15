d2cc7b2ea4c00927c91b290f695d14d0853b41d2
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:57:45 +0000

    add Eamon Highwind, 0x9e4faf188764f43aac37f51ecdedc1e8c6611b106061a8a8c55291cb7215ee20, armor, bot_add_nfts_2023_09_15T22_54_24

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -2081,7 +2081,8 @@ Types = {
         NFT_9bf667db4012f265403c1cc42c980a71bb1007cd4cb93554ce2393a1ceb5ebe0: 1894,
         NFT_9bfcb638eb5f46baa326f84fb495baf97424ba5169caa211c770aef70c9c2f03: 1895,
         NFT_9c6465b08bf2f98ed56eeccf47b846f1ece2afdce6cbfb3c2c849e2f028998c6: 1896,
-        NFT_9e3bd25b2f782ba03e64daa0684a955dc77595db2c6ae4486aefc3f8c9cf5ec9: 1897 // @lastid@
+        NFT_9e3bd25b2f782ba03e64daa0684a955dc77595db2c6ae4486aefc3f8c9cf5ec9: 1897,
+        NFT_9e4faf188764f43aac37f51ecdedc1e8c6611b106061a8a8c55291cb7215ee20: 1898 // @lastid@
         //@nextIdLine@
     },
     
@@ -4159,6 +4160,7 @@ var kinds = {
     NFT_9bfcb638eb5f46baa326f84fb495baf97424ba5169caa211c770aef70c9c2f03: [Types.Entities.NFT_9bfcb638eb5f46baa326f84fb495baf97424ba5169caa211c770aef70c9c2f03, "weapon"],
     NFT_9c6465b08bf2f98ed56eeccf47b846f1ece2afdce6cbfb3c2c849e2f028998c6: [Types.Entities.NFT_9c6465b08bf2f98ed56eeccf47b846f1ece2afdce6cbfb3c2c849e2f028998c6, "weapon"],
     NFT_9e3bd25b2f782ba03e64daa0684a955dc77595db2c6ae4486aefc3f8c9cf5ec9: [Types.Entities.NFT_9e3bd25b2f782ba03e64daa0684a955dc77595db2c6ae4486aefc3f8c9cf5ec9, "weapon"],
+    NFT_9e4faf188764f43aac37f51ecdedc1e8c6611b106061a8a8c55291cb7215ee20: [Types.Entities.NFT_9e4faf188764f43aac37f51ecdedc1e8c6611b106061a8a8c55291cb7215ee20, "armor"],
     // @nextSpriteLine@
 
     
