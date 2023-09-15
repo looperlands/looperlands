b70c8aeb4522987a8c0ad2ea99e409df1e56cc82
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:55:50 +0000

    add Emerald Voidreaver, 0x566f35f1168a3119302f23385457826094f9204c01d3bf7b2058273c99d9c3df, weapon, bot_add_nfts_2023_09_15T22_54_24

--- client/js/game.js
+++ client/js/game.js
@@ -1806,6 +1806,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_53b5248ede334c73f83eb075789fc19f7fbf0493a0fa28b1df6ee3348686eeeb",
                                 "NFT_53efc82e190b6d085ac11a4599a15ca2c1f4d54284fc383d787340f2a7cb91e7",
                                 "NFT_5466747702b32d5da13b30b8c50d9b3e67d2efeb1f4727fd803018b8c3272bc4",
+                                "NFT_566f35f1168a3119302f23385457826094f9204c01d3bf7b2058273c99d9c3df",
                                 // @nextSpriteLine@
                             ];                          
         },
