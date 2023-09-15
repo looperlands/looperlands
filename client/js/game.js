0a8441fdc3cb0c60d10f18b714b7e026e69b6c76
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:59:52 +0000

    add Golden Reaver, 0xef6266909a33a0a4d15e9329fa07bbebc5a7353b6334167a458777294f79d1d6, weapon, bot_add_nfts_2023_09_15T22_54_24

--- client/js/game.js
+++ client/js/game.js
@@ -2067,6 +2067,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_ee20b217cb12798b69c400e43ba5c95f6d55a84f7d3be633977e92be65f989e5",
                                 "NFT_ee82353da2f4fd11769705ca15d4fb2c974a3449db332828962cf20c1936883d",
                                 "NFT_eee46bff0fd89a12d35b9141c05729eec8b20675f654fcdfb2a6ed568185d6f7",
+                                "NFT_ef6266909a33a0a4d15e9329fa07bbebc5a7353b6334167a458777294f79d1d6",
                                 // @nextSpriteLine@
                             ];                          
         },
