2b6cbd99b3accbcc253049e78a9a6a2dc5e162e2
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:56:10 +0000

    add Amythest Deathwhisper, 0x616a1cdcc8c7bc2192034715b6fdf71ee939e4b1c225ca9713d73aade44dd034, weapon, bot_add_nfts_2023_09_15T22_54_24

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1947,6 +1947,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_6100929c73a7de398e3251a647ff1e567f4aa8ca5805fe10e38e90deabda8ad5.json',
         'text!../sprites/NFT_61346c17f3232ee6321d03530ec434d77cc797be64b781d798ef33974ddba03f.json',
         'text!../sprites/NFT_614f187631b1827d105a4c06237dae3e0c4c8ba42d92d3179a0fb4d9e16b1efc.json',
+        'text!../sprites/NFT_616a1cdcc8c7bc2192034715b6fdf71ee939e4b1c225ca9713d73aade44dd034.json',
         // @nextSpriteImport@
     ], function() {
     
