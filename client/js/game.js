9d5db293f4338cecddf9aaee2431def8ac08fce8
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:55:31 +0000

    add Dragga Swiftfoot, 0x467cd21ee0c4fc8bcf9d58d5a5642a1e1b63c897f7c2953f0053417e2c813ae2, armor, bot_add_nfts_2023_09_15T22_54_24

--- client/js/game.js
+++ client/js/game.js
@@ -1786,6 +1786,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_44741d7f36397eaeadf4a4e594bd054c23b62919f117eee9b93320b4a0b804a6",
                                 "NFT_449b2e895a1f7d0c5c06f2edfeb6ca6c9aeefd92796e4f68c32662aab82d7195",
                                 "NFT_45aa0d2385d24e2365f3662a91059dd6c1814f5edff7c6e824d25a627d245225",
+                                "NFT_467cd21ee0c4fc8bcf9d58d5a5642a1e1b63c897f7c2953f0053417e2c813ae2",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1903,6 +1903,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_44741d7f36397eaeadf4a4e594bd054c23b62919f117eee9b93320b4a0b804a6.json',
         'text!../sprites/NFT_449b2e895a1f7d0c5c06f2edfeb6ca6c9aeefd92796e4f68c32662aab82d7195.json',
         'text!../sprites/NFT_45aa0d2385d24e2365f3662a91059dd6c1814f5edff7c6e824d25a627d245225.json',
+        'text!../sprites/NFT_467cd21ee0c4fc8bcf9d58d5a5642a1e1b63c897f7c2953f0053417e2c813ae2.json',
         // @nextSpriteImport@
     ], function() {
     
