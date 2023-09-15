5711e21beb5b1754cba6158c8fca23ea8db517ad
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:56:14 +0000

    add Gold Prismagic Staff, 0x6268294e620c03ec9084a5bf5abe2e3ad07a795e50836a88ba320eec135b8009, weapon, bot_add_nfts_2023_09_15T22_54_24

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1952,6 +1952,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_61f54783e72af190839194be64cb5e0ee6eb0646e76697c66481bc5767cfbbc4.json',
         'text!../sprites/NFT_6216713721e0a4845c7ad60539578f8576cbfd55cb569ff77b8b90260b2e6d69.json',
         'text!../sprites/NFT_6230ebb6e3e4687fc88b4b333e8a4017ae76b0d197931a376060024bd1843bdc.json',
+        'text!../sprites/NFT_6268294e620c03ec9084a5bf5abe2e3ad07a795e50836a88ba320eec135b8009.json',
         // @nextSpriteImport@
     ], function() {
     
