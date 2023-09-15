de6547b7aed26b453abc248af3590e7e1c9144b8
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:56:57 +0000

    add Legacys Edge, 0x7c727ed496e450a0a440e9ae0cf86956ddf555a15fb7bcf0a71fadb5289047b0, weapon, bot_add_nfts_2023_09_15T22_54_24

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -2027,7 +2027,8 @@ Types = {
         NFT_7ad2d21dc7b63554e92afabe010e45ff50d0c584ea9a8bdb5695217ce4acf91e: 1840,
         NFT_7ae623a460e649368b695c832d71235a960c45b3bfc664ba8aae17f6b2b41490: 1841,
         NFT_7c2165364cc9d74b6bea85d2d68a396efb1b9298f665d8aea214d72589eb49f3: 1842,
-        NFT_7c71ee7666c23bedebda30b8830be559939df0d3afec4e280e1dc4e2e1245836: 1843 // @lastid@
+        NFT_7c71ee7666c23bedebda30b8830be559939df0d3afec4e280e1dc4e2e1245836: 1843,
+        NFT_7c727ed496e450a0a440e9ae0cf86956ddf555a15fb7bcf0a71fadb5289047b0: 1844 // @lastid@
         //@nextIdLine@
     },
     
@@ -4051,6 +4052,7 @@ var kinds = {
     NFT_7ae623a460e649368b695c832d71235a960c45b3bfc664ba8aae17f6b2b41490: [Types.Entities.NFT_7ae623a460e649368b695c832d71235a960c45b3bfc664ba8aae17f6b2b41490, "weapon"],
     NFT_7c2165364cc9d74b6bea85d2d68a396efb1b9298f665d8aea214d72589eb49f3: [Types.Entities.NFT_7c2165364cc9d74b6bea85d2d68a396efb1b9298f665d8aea214d72589eb49f3, "armor"],
     NFT_7c71ee7666c23bedebda30b8830be559939df0d3afec4e280e1dc4e2e1245836: [Types.Entities.NFT_7c71ee7666c23bedebda30b8830be559939df0d3afec4e280e1dc4e2e1245836, "armor"],
+    NFT_7c727ed496e450a0a440e9ae0cf86956ddf555a15fb7bcf0a71fadb5289047b0: [Types.Entities.NFT_7c727ed496e450a0a440e9ae0cf86956ddf555a15fb7bcf0a71fadb5289047b0, "weapon"],
     // @nextSpriteLine@
 
     
