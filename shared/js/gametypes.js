bd0fe69bcd9f351e6cf44aa8e253cd7bdd21fc82
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:57:46 +0000

    add Anduril, 0xa113f4a057621af49d4895c7a7f5bb776c0771566745bb74255d901cab2fe9e6, weapon, bot_add_nfts_2023_09_15T22_54_24

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -2083,7 +2083,8 @@ Types = {
         NFT_9c6465b08bf2f98ed56eeccf47b846f1ece2afdce6cbfb3c2c849e2f028998c6: 1896,
         NFT_9e3bd25b2f782ba03e64daa0684a955dc77595db2c6ae4486aefc3f8c9cf5ec9: 1897,
         NFT_9e4faf188764f43aac37f51ecdedc1e8c6611b106061a8a8c55291cb7215ee20: 1898,
-        NFT_9ff9d7406dea63077570d3125cdd5eb8db83ce8fa030ac84de168905b3402cec: 1899 // @lastid@
+        NFT_9ff9d7406dea63077570d3125cdd5eb8db83ce8fa030ac84de168905b3402cec: 1899,
+        NFT_a113f4a057621af49d4895c7a7f5bb776c0771566745bb74255d901cab2fe9e6: 1900 // @lastid@
         //@nextIdLine@
     },
     
@@ -4163,6 +4164,7 @@ var kinds = {
     NFT_9e3bd25b2f782ba03e64daa0684a955dc77595db2c6ae4486aefc3f8c9cf5ec9: [Types.Entities.NFT_9e3bd25b2f782ba03e64daa0684a955dc77595db2c6ae4486aefc3f8c9cf5ec9, "weapon"],
     NFT_9e4faf188764f43aac37f51ecdedc1e8c6611b106061a8a8c55291cb7215ee20: [Types.Entities.NFT_9e4faf188764f43aac37f51ecdedc1e8c6611b106061a8a8c55291cb7215ee20, "armor"],
     NFT_9ff9d7406dea63077570d3125cdd5eb8db83ce8fa030ac84de168905b3402cec: [Types.Entities.NFT_9ff9d7406dea63077570d3125cdd5eb8db83ce8fa030ac84de168905b3402cec, "weapon"],
+    NFT_a113f4a057621af49d4895c7a7f5bb776c0771566745bb74255d901cab2fe9e6: [Types.Entities.NFT_a113f4a057621af49d4895c7a7f5bb776c0771566745bb74255d901cab2fe9e6, "weapon"],
     // @nextSpriteLine@
 
     
