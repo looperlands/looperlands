eeec7a44dc9391e87ab8c5ac860dff3cf514ed44
loopwormsbot <balkshamster@proton.me>
2023-09-15 22:55:33 +0000

    add Oswin Frostfire, 0x492b76e44bf155b7cf1137ea2a40a7bd5af50b8b474113c13192db2208a70394, armor, bot_add_nfts_2023_09_15T22_54_24

--- client/js/game.js
+++ client/js/game.js
@@ -1788,6 +1788,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_45aa0d2385d24e2365f3662a91059dd6c1814f5edff7c6e824d25a627d245225",
                                 "NFT_467cd21ee0c4fc8bcf9d58d5a5642a1e1b63c897f7c2953f0053417e2c813ae2",
                                 "NFT_476205408b25bf8be4774e1ea0de2ad63425ed1abcfb6577656391bacdf36b33",
+                                "NFT_492b76e44bf155b7cf1137ea2a40a7bd5af50b8b474113c13192db2208a70394",
                                 // @nextSpriteLine@
                             ];                          
         },
