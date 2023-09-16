21b1765ad64780e83260e6988d5733867381884b
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:05:30 +0000

    add Ruk Flamehair, 0x822a398c216b6e43a1cdd8ea5c885648020dd7cd252d3e9eee524b92df1555ab, armor, bot_add_nfts_2023_09_16T10_02_10

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2079,6 +2079,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_7f3bbe437cce2c9f245532b9b0d0d466777bf0a354b97c5475e806060d235977.json',
         'text!../sprites/NFT_8025e58c20f8eca2bb352188bac543415c3b99c337e0979aab68555840dffc5a.json',
         'text!../sprites/NFT_815cd179324d25bd1de64c0b055e20e160b9f99460ef70b8ff11683ee94ebc56.json',
+        'text!../sprites/NFT_822a398c216b6e43a1cdd8ea5c885648020dd7cd252d3e9eee524b92df1555ab.json',
         // @nextSpriteImport@
     ], function() {
     
