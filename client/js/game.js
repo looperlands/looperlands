0804399dfe03033ae6371fa1d75edd580b54439c
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:02:38 +0000

    add Pounce Quickfoot, 0x0f28b244e6951a81544b0eb68794e5e3a8842fb9aa04ad311933b0cd70f334db, armor, bot_add_nfts_2023_09_16T10_02_10

--- client/js/game.js
+++ client/js/game.js
@@ -1754,6 +1754,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_0eddd5e7f2f6598fed9be0f1e26e8b954e40cc18de008c2759d2b0108387a1a8",
                                 "NFT_0ee407ab3c98995829ac800fbc7661966f9af9973ce425e7a8ca4ff126062581",
                                 "NFT_0ef9ca6d7261c8732eaa554604e963474889d2aca146c4e696820236694796a0",
+                                "NFT_0f28b244e6951a81544b0eb68794e5e3a8842fb9aa04ad311933b0cd70f334db",
                                 // @nextSpriteLine@
                             ];                          
         },
