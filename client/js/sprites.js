6b5aeddafda9ca45d07cf7f3fe91c8eb1d8389a4
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:06:25 +0000

    add Frost Javelin, 0xadb783028b512ce715f755d08b906a4706f1b40c8b0b443d763120f12d3dd854, weapon, bot_add_nfts_2023_09_16T10_02_10

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2139,6 +2139,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_a9149df3bd4d90ef4dae8244c9cff5e6fac5ce53775d405da294b9e9bd34d04e.json',
         'text!../sprites/NFT_a9200375f46ae75386010960565d3fb651099491966a10ed1b2fe17de3e59132.json',
         'text!../sprites/NFT_a941e7a1a810cca6bd485f23e143f722a133bf8fdbd4b74f3cc7b2275732ab15.json',
+        'text!../sprites/NFT_adb783028b512ce715f755d08b906a4706f1b40c8b0b443d763120f12d3dd854.json',
         // @nextSpriteImport@
     ], function() {
     
