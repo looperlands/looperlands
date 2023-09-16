fca2dd5cceef013ce7b648c0a55c69bbeec72459
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:04:38 +0000

    add Thaddeus Windwhisper, 0x61346c17f3232ee6321d03530ec434d77cc797be64b781d798ef33974ddba03f, armor, bot_add_nfts_2023_09_16T10_02_10

--- client/js/game.js
+++ client/js/game.js
@@ -1900,6 +1900,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_60b89d2586f7637305be7d442974613b395cd35e7ed52c54c26f00665a889128",
                                 "NFT_60f0b84816222b4abc877cd5f44c6a75d4f22756228e45147111f5a2dc90059c",
                                 "NFT_6100929c73a7de398e3251a647ff1e567f4aa8ca5805fe10e38e90deabda8ad5",
+                                "NFT_61346c17f3232ee6321d03530ec434d77cc797be64b781d798ef33974ddba03f",
                                 // @nextSpriteLine@
                             ];                          
         },
