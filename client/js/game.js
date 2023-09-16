7ea610b962937fca59c7c92de5af57ebfc025d5d
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:12:45 +0000

    add Divine Reckoning, 0xdbe8cfa1f75a18c4b064a51a1a8ea148ddf0a73a053cb36d9be9d0b9c95436b2, weapon, bot_add_nfts_2023_09_16T10_02_10

--- client/js/game.js
+++ client/js/game.js
@@ -2098,6 +2098,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_dab7e2b47212677d9746717f399a0f3e1fc2c8abf4ad4fca509e6a12696b031a",
                                 "NFT_daed3c471c87b2ac546fb61d5e1faf0b92b761556aa08005fa750fcc00862095",
                                 "NFT_db9a70c104136f43ddc236fc7bb0b5fed62c8c00108e167b5da115a0583de0cd",
+                                "NFT_dbe8cfa1f75a18c4b064a51a1a8ea148ddf0a73a053cb36d9be9d0b9c95436b2",
                                 // @nextSpriteLine@
                             ];                          
         },
