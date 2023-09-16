c128f64c68e0fdec3261b39032bdd55b93310237
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:05:24 +0000

    add Legacys Edge, 0x7c727ed496e450a0a440e9ae0cf86956ddf555a15fb7bcf0a71fadb5289047b0, weapon, bot_add_nfts_2023_09_16T10_02_10

--- client/js/game.js
+++ client/js/game.js
@@ -1954,6 +1954,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_7ae623a460e649368b695c832d71235a960c45b3bfc664ba8aae17f6b2b41490",
                                 "NFT_7c2165364cc9d74b6bea85d2d68a396efb1b9298f665d8aea214d72589eb49f3",
                                 "NFT_7c71ee7666c23bedebda30b8830be559939df0d3afec4e280e1dc4e2e1245836",
+                                "NFT_7c727ed496e450a0a440e9ae0cf86956ddf555a15fb7bcf0a71fadb5289047b0",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2071,6 +2071,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_7ae623a460e649368b695c832d71235a960c45b3bfc664ba8aae17f6b2b41490.json',
         'text!../sprites/NFT_7c2165364cc9d74b6bea85d2d68a396efb1b9298f665d8aea214d72589eb49f3.json',
         'text!../sprites/NFT_7c71ee7666c23bedebda30b8830be559939df0d3afec4e280e1dc4e2e1245836.json',
+        'text!../sprites/NFT_7c727ed496e450a0a440e9ae0cf86956ddf555a15fb7bcf0a71fadb5289047b0.json',
         // @nextSpriteImport@
     ], function() {
     
