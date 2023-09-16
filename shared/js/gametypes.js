ad549321a792deff5aaa9abe1b6219531f9e48a9
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:05:41 +0000

    add Caspar Emberpeak, 0x8963932f98bdcc50886ff42a444e5ad5e2bf820838254ba9c1518c9a1bf174cb, armor, bot_add_nfts_2023_09_16T10_02_10

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -2119,7 +2119,8 @@ Types = {
         NFT_887394d15316b881041b53e24fd1665082c37f723165359f950e0511b4a8c328: 1932,
         NFT_889de1734d23a930cb48692a2e64772f3f1a1e48316398c54eeb5681e7e589ea: 1933,
         NFT_88f41bde9558200636a5e22507b9f4bb14586a4bdbe4028fb015de654f4ddbdf: 1934,
-        NFT_89233f3366f15b841deae4488dffa4bee6bb6c7dbb94f7a5a3db4389bfc88156: 1935 // @lastid@
+        NFT_89233f3366f15b841deae4488dffa4bee6bb6c7dbb94f7a5a3db4389bfc88156: 1935,
+        NFT_8963932f98bdcc50886ff42a444e5ad5e2bf820838254ba9c1518c9a1bf174cb: 1936 // @lastid@
         //@nextIdLine@
     },
     
@@ -4235,6 +4236,7 @@ var kinds = {
     NFT_889de1734d23a930cb48692a2e64772f3f1a1e48316398c54eeb5681e7e589ea: [Types.Entities.NFT_889de1734d23a930cb48692a2e64772f3f1a1e48316398c54eeb5681e7e589ea, "weapon"],
     NFT_88f41bde9558200636a5e22507b9f4bb14586a4bdbe4028fb015de654f4ddbdf: [Types.Entities.NFT_88f41bde9558200636a5e22507b9f4bb14586a4bdbe4028fb015de654f4ddbdf, "armor"],
     NFT_89233f3366f15b841deae4488dffa4bee6bb6c7dbb94f7a5a3db4389bfc88156: [Types.Entities.NFT_89233f3366f15b841deae4488dffa4bee6bb6c7dbb94f7a5a3db4389bfc88156, "weapon"],
+    NFT_8963932f98bdcc50886ff42a444e5ad5e2bf820838254ba9c1518c9a1bf174cb: [Types.Entities.NFT_8963932f98bdcc50886ff42a444e5ad5e2bf820838254ba9c1518c9a1bf174cb, "armor"],
     // @nextSpriteLine@
 
     
