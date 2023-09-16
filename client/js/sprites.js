719b14d9d7c1efa2939e17f43d55bfd276b2e0c3
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:05:12 +0000

    add Vyrori Tlabbar, 0x7383645681fe62974961d89d9b21833d5200c0af763a56d2263fca41fcbc355b, armor, bot_add_nfts_2023_09_16T10_02_10

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2056,6 +2056,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_71ae200619c4e118f482871f83e1556d101e0fcc9c3f9ce2ad1a68aac29a52b5.json',
         'text!../sprites/NFT_71f79d7e9909c607379b40fa8571b7671e82ba0cea81c7e3c97663efeee82403.json',
         'text!../sprites/NFT_722d6ec8e69aa0f5148b47507a59acf10b74274900055823419a2086e6603e33.json',
+        'text!../sprites/NFT_7383645681fe62974961d89d9b21833d5200c0af763a56d2263fca41fcbc355b.json',
         // @nextSpriteImport@
     ], function() {
     
