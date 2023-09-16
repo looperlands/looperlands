fd2bac0b1cb4fa605ebb6e218fc1e2c58c6ae3c2
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:05:59 +0000

    add Adeline Nightgale, 0x94ed94ebfe9dac19e64a1327fdc1e9ada07aa984179ae1612a7f8b840ee9d155, armor, bot_add_nfts_2023_09_16T10_02_10

--- client/js/game.js
+++ client/js/game.js
@@ -1994,6 +1994,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_918a1f502d2257037c47a1d0c8da6511c83393485670a36b27372e025ce9e83a",
                                 "NFT_92a7d3c96f3e237eaffe0280605479a3df00a7f7e866c8e7d3174859b4c52f85",
                                 "NFT_948cc367cc0bc338332caac2b861ac3fc5010f791ab940bd9f54c9902eb41357",
+                                "NFT_94ed94ebfe9dac19e64a1327fdc1e9ada07aa984179ae1612a7f8b840ee9d155",
                                 // @nextSpriteLine@
                             ];                          
         },
