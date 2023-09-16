62b1342e61ff1a1b017dbe9669775e94ce2fc0ee
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:13:40 +0000

    add Emerald Thornweave Blade, 0xf8fb0cb8dc176a2c57207360b55dedb1398bd01634e4d0b150a2df0300e4cd7d, weapon, bot_add_nfts_2023_09_16T10_02_10

--- client/js/game.js
+++ client/js/game.js
@@ -2155,6 +2155,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_f7fb9c285cf34bb8bfd72b7564138c4781d8e369b6f1307f012bf7cbd5eb2ad2",
                                 "NFT_f7fc080ee9c7c2e248f1c6c1a654a9cdf30a0d85ba35d69b8bc1f9a438616769",
                                 "NFT_f8e85410927a306e1c382287f6d18a168f9d8499583382c42e3621d275469a4d",
+                                "NFT_f8fb0cb8dc176a2c57207360b55dedb1398bd01634e4d0b150a2df0300e4cd7d",
                                 // @nextSpriteLine@
                             ];                          
         },
