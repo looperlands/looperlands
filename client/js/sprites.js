214a0f9a66d69689df1c308e3ed8e55e4dd788c5
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:03:52 +0000

    add Dominic Sunsword, 0x45aa0d2385d24e2365f3662a91059dd6c1814f5edff7c6e824d25a627d245225, armor, bot_add_nfts_2023_09_16T10_02_10

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1961,6 +1961,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_44741d7f36397eaeadf4a4e594bd054c23b62919f117eee9b93320b4a0b804a6.json',
         'text!../sprites/NFT_447854ba4464fa4dc0a7046bcc1a987b1afaf40ee4de2a55c67bfe458e9c8a87.json',
         'text!../sprites/NFT_449b2e895a1f7d0c5c06f2edfeb6ca6c9aeefd92796e4f68c32662aab82d7195.json',
+        'text!../sprites/NFT_45aa0d2385d24e2365f3662a91059dd6c1814f5edff7c6e824d25a627d245225.json',
         // @nextSpriteImport@
     ], function() {
     
--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -1989,7 +1989,8 @@ Types = {
         NFT_4429e0af5cf8407e9f7a7472e892cca4c5388d95f2eee3eebe491a245863b497: 1802,
         NFT_44741d7f36397eaeadf4a4e594bd054c23b62919f117eee9b93320b4a0b804a6: 1803,
         NFT_447854ba4464fa4dc0a7046bcc1a987b1afaf40ee4de2a55c67bfe458e9c8a87: 1804,
-        NFT_449b2e895a1f7d0c5c06f2edfeb6ca6c9aeefd92796e4f68c32662aab82d7195: 1805 // @lastid@
+        NFT_449b2e895a1f7d0c5c06f2edfeb6ca6c9aeefd92796e4f68c32662aab82d7195: 1805,
+        NFT_45aa0d2385d24e2365f3662a91059dd6c1814f5edff7c6e824d25a627d245225: 1806 // @lastid@
         //@nextIdLine@
     },
     
@@ -3975,6 +3976,7 @@ var kinds = {
     NFT_44741d7f36397eaeadf4a4e594bd054c23b62919f117eee9b93320b4a0b804a6: [Types.Entities.NFT_44741d7f36397eaeadf4a4e594bd054c23b62919f117eee9b93320b4a0b804a6, "armor"],
     NFT_447854ba4464fa4dc0a7046bcc1a987b1afaf40ee4de2a55c67bfe458e9c8a87: [Types.Entities.NFT_447854ba4464fa4dc0a7046bcc1a987b1afaf40ee4de2a55c67bfe458e9c8a87, "weapon"],
     NFT_449b2e895a1f7d0c5c06f2edfeb6ca6c9aeefd92796e4f68c32662aab82d7195: [Types.Entities.NFT_449b2e895a1f7d0c5c06f2edfeb6ca6c9aeefd92796e4f68c32662aab82d7195, "armor"],
+    NFT_45aa0d2385d24e2365f3662a91059dd6c1814f5edff7c6e824d25a627d245225: [Types.Entities.NFT_45aa0d2385d24e2365f3662a91059dd6c1814f5edff7c6e824d25a627d245225, "armor"],
     // @nextSpriteLine@
 
     
