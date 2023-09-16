2cc101e5f5c4a21cb506bd1256c54bab51bf7aa6
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:02:22 +0000

    add Stormbreaker, 0x0706c565bef9feb85f4799609a6ceee6b01b50593b0e34f179560580e0c42a66, weapon, bot_add_nfts_2023_09_16T10_02_10

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -1854,6 +1854,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_06339326463d3c2734ead904a31dae5fa3f260742e4efb10afb0bfe8fdc81b34.json',
         'text!../sprites/NFT_065562c3cc76066a783e036e0e52d7dbd3157ec40a6a30ca29445a0ee07a677a.json',
         'text!../sprites/NFT_06e6b598fabfd1dbe58ec7c018bc2c812255627c1a689f149734166d8127a811.json',
+        'text!../sprites/NFT_0706c565bef9feb85f4799609a6ceee6b01b50593b0e34f179560580e0c42a66.json',
         // @nextSpriteImport@
     ], function() {
     
