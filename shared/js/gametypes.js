2ddf43a18d6d89bb6f0c7524e14f29a07400efac
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:13:28 +0000

    add Frost Solarchron, 0xf27509d233fcc5d70cd8ae4a485891c4dbcc047d493e0e94aece32a7677de0f4, weapon, bot_add_nfts_2023_09_16T10_02_10

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -2288,7 +2288,8 @@ Types = {
         NFT_effc7ad6cf1ffb7bb878ddd7cf342b21fd13fbf6a7b09d0cd4e3d4abc7368bc8: 2101,
         NFT_f03596fe1e47e8cad64140e03a8e9428ed6b38dd6dcc564e9abc48b5a9103918: 2102,
         NFT_f03cb11ee8f6e5b945af52a6c5f36a90572c55b65b874d4cc6e36e271ad27624: 2103,
-        NFT_f08d89a193a4604455ac7a48b0aeda446bdcc569b6ec8ea4a6b040881b35f356: 2104 // @lastid@
+        NFT_f08d89a193a4604455ac7a48b0aeda446bdcc569b6ec8ea4a6b040881b35f356: 2104,
+        NFT_f27509d233fcc5d70cd8ae4a485891c4dbcc047d493e0e94aece32a7677de0f4: 2105 // @lastid@
         //@nextIdLine@
     },
     
@@ -4573,6 +4574,7 @@ var kinds = {
     NFT_f03596fe1e47e8cad64140e03a8e9428ed6b38dd6dcc564e9abc48b5a9103918: [Types.Entities.NFT_f03596fe1e47e8cad64140e03a8e9428ed6b38dd6dcc564e9abc48b5a9103918, "weapon"],
     NFT_f03cb11ee8f6e5b945af52a6c5f36a90572c55b65b874d4cc6e36e271ad27624: [Types.Entities.NFT_f03cb11ee8f6e5b945af52a6c5f36a90572c55b65b874d4cc6e36e271ad27624, "weapon"],
     NFT_f08d89a193a4604455ac7a48b0aeda446bdcc569b6ec8ea4a6b040881b35f356: [Types.Entities.NFT_f08d89a193a4604455ac7a48b0aeda446bdcc569b6ec8ea4a6b040881b35f356, "weapon"],
+    NFT_f27509d233fcc5d70cd8ae4a485891c4dbcc047d493e0e94aece32a7677de0f4: [Types.Entities.NFT_f27509d233fcc5d70cd8ae4a485891c4dbcc047d493e0e94aece32a7677de0f4, "weapon"],
     // @nextSpriteLine@
 
     
