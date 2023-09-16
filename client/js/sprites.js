c45365eae0dd63e608b406452779e5ee0a0414fd
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:06:31 +0000

    add Ephraim Ironheart, 0xb25a8c1d7041819b64644d5362086643c1d1466715cd140c9f5f3e3618a146d3, armor, bot_add_nfts_2023_09_16T10_02_10

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2146,6 +2146,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_b0b07a5c28d447cbff21e85340a76ff9588f7f07eff84d52ebe7a8f61ba838d6.json',
         'text!../sprites/NFT_b1b589675a828caf90fa75f13d45c47d3d973d506ac9bb7b9b9d1f64b36bc3dc.json',
         'text!../sprites/NFT_b1e4f851070a70b423a90e16831d92c002e150056a7187edf518ccb4682c62b7.json',
+        'text!../sprites/NFT_b25a8c1d7041819b64644d5362086643c1d1466715cd140c9f5f3e3618a146d3.json',
         // @nextSpriteImport@
     ], function() {
     
