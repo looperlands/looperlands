e3ace922f8a255f0dd5b33e00e6e113d1af1b555
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:05:57 +0000

    add Zogar Bonebreaker, 0x92a7d3c96f3e237eaffe0280605479a3df00a7f7e866c8e7d3174859b4c52f85, armor, bot_add_nfts_2023_09_16T10_02_10

--- client/js/game.js
+++ client/js/game.js
@@ -1992,6 +1992,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_8f8d9f8bcde9b9f8ddc476975b568afb36262b3631899fe13d93c4c6063c464e",
                                 "NFT_909fc50ad8fa0f22baaa9b7cf044c61b15744a21b576b46304e9b907e61d89f5",
                                 "NFT_918a1f502d2257037c47a1d0c8da6511c83393485670a36b27372e025ce9e83a",
+                                "NFT_92a7d3c96f3e237eaffe0280605479a3df00a7f7e866c8e7d3174859b4c52f85",
                                 // @nextSpriteLine@
                             ];                          
         },
