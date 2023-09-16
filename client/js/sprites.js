52cec1145af311d192166bde2ee9dffd9b98d2d8
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:06:06 +0000

    add Sunforged Tempest Spear, 0x9ad46acdf36898759605bde56b54dcc83e343f65e8b53d9650cd41f12773fd88, weapon, bot_add_nfts_2023_09_16T10_02_10

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2118,6 +2118,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_9960323dc195786f188d1f9c6ace18e14ebeb2f4042aa4f43d242d1dd1f29d20.json',
         'text!../sprites/NFT_99626aee07ac2275d5b279a456342a46d191149525e61ef3646dac4268331fca.json',
         'text!../sprites/NFT_9a087f89920976a336f8f3c2bd8e878b22b2293046217b0c00f6adaafaaad90a.json',
+        'text!../sprites/NFT_9ad46acdf36898759605bde56b54dcc83e343f65e8b53d9650cd41f12773fd88.json',
         // @nextSpriteImport@
     ], function() {
     
