a170c6f8b2e78751008f2127dd095902229e0dd2
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:12:46 +0000

    add Amethyst Mallet, 0xdc7b2f4156e4dfd9f8ab202c4db5663545a3d13aba296fa5434bab0bab408ce3, weapon, bot_add_nfts_2023_09_16T10_02_10

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -2244,7 +2244,8 @@ Types = {
         NFT_dab7e2b47212677d9746717f399a0f3e1fc2c8abf4ad4fca509e6a12696b031a: 2057,
         NFT_daed3c471c87b2ac546fb61d5e1faf0b92b761556aa08005fa750fcc00862095: 2058,
         NFT_db9a70c104136f43ddc236fc7bb0b5fed62c8c00108e167b5da115a0583de0cd: 2059,
-        NFT_dbe8cfa1f75a18c4b064a51a1a8ea148ddf0a73a053cb36d9be9d0b9c95436b2: 2060 // @lastid@
+        NFT_dbe8cfa1f75a18c4b064a51a1a8ea148ddf0a73a053cb36d9be9d0b9c95436b2: 2060,
+        NFT_dc7b2f4156e4dfd9f8ab202c4db5663545a3d13aba296fa5434bab0bab408ce3: 2061 // @lastid@
         //@nextIdLine@
     },
     
@@ -4485,6 +4486,7 @@ var kinds = {
     NFT_daed3c471c87b2ac546fb61d5e1faf0b92b761556aa08005fa750fcc00862095: [Types.Entities.NFT_daed3c471c87b2ac546fb61d5e1faf0b92b761556aa08005fa750fcc00862095, "armor"],
     NFT_db9a70c104136f43ddc236fc7bb0b5fed62c8c00108e167b5da115a0583de0cd: [Types.Entities.NFT_db9a70c104136f43ddc236fc7bb0b5fed62c8c00108e167b5da115a0583de0cd, "weapon"],
     NFT_dbe8cfa1f75a18c4b064a51a1a8ea148ddf0a73a053cb36d9be9d0b9c95436b2: [Types.Entities.NFT_dbe8cfa1f75a18c4b064a51a1a8ea148ddf0a73a053cb36d9be9d0b9c95436b2, "weapon"],
+    NFT_dc7b2f4156e4dfd9f8ab202c4db5663545a3d13aba296fa5434bab0bab408ce3: [Types.Entities.NFT_dc7b2f4156e4dfd9f8ab202c4db5663545a3d13aba296fa5434bab0bab408ce3, "weapon"],
     // @nextSpriteLine@
 
     
