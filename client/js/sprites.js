2c119eab041a62400d67bd2fa1ce80027b5c4e6e
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:06:31 +0000

    add Frost Oaksplitter, 0xb2c1ecefd044b36ea10a1e26754d5816780415eb3d983921f5678f2040101004, weapon, bot_add_nfts_2023_09_16T10_02_10

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2147,6 +2147,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_b1b589675a828caf90fa75f13d45c47d3d973d506ac9bb7b9b9d1f64b36bc3dc.json',
         'text!../sprites/NFT_b1e4f851070a70b423a90e16831d92c002e150056a7187edf518ccb4682c62b7.json',
         'text!../sprites/NFT_b25a8c1d7041819b64644d5362086643c1d1466715cd140c9f5f3e3618a146d3.json',
+        'text!../sprites/NFT_b2c1ecefd044b36ea10a1e26754d5816780415eb3d983921f5678f2040101004.json',
         // @nextSpriteImport@
     ], function() {
     
