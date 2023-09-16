f0c8846a451d5176b6ccc096a143387f7e69b440
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:04:53 +0000

    add Frostbite, 0x69f50440fdcc6c5ac2dfdac4ae88b8e8e49f0cedf196a098098189357ee703f2, weapon, bot_add_nfts_2023_09_16T10_02_10

--- client/js/game.js
+++ client/js/game.js
@@ -1920,6 +1920,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_67c8a949e36419c312a23f0fa85707f4b7d5ef6abd962b748b84cf7bd91eb7dd",
                                 "NFT_68d7b8dd433f959c7830ae0a6c075895fa52c926a8eec860b82f02ba255370f6",
                                 "NFT_69f13b9df75c3fb8ebf83a3c7f5abb1d85b956037415368fb86e35def6533e5e",
+                                "NFT_69f50440fdcc6c5ac2dfdac4ae88b8e8e49f0cedf196a098098189357ee703f2",
                                 // @nextSpriteLine@
                             ];                          
         },
