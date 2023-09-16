4dbed5d42c0bf4c9be8fb434d4eae5496310e677
loopwormsbot <balkshamster@proton.me>
2023-09-16 10:04:36 +0000

    add Sunforged Celestial Staff, 0x60f0b84816222b4abc877cd5f44c6a75d4f22756228e45147111f5a2dc90059c, weapon, bot_add_nfts_2023_09_16T10_02_10

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2015,6 +2015,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_5e86e0590a987ea826ed947bfc03bd8e7b78c5e05341a5d7b8938b636bc9a9d8.json',
         'text!../sprites/NFT_5f8c269ab242adcff33192b0763b52da150095c669b32c221a674c047671c589.json',
         'text!../sprites/NFT_60b89d2586f7637305be7d442974613b395cd35e7ed52c54c26f00665a889128.json',
+        'text!../sprites/NFT_60f0b84816222b4abc877cd5f44c6a75d4f22756228e45147111f5a2dc90059c.json',
         // @nextSpriteImport@
     ], function() {
     
