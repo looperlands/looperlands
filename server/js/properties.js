8ed64babfb18fac2896982c9adb8daf5f9cf2901
ordinaryadam <joshiadam@hotmail.com>
2023-09-16 19:26:16 -0700

    upd

--- server/js/properties.js
+++ server/js/properties.js
@@ -160,9 +160,9 @@ var Properties = {
             potion: 100,
             
         },
-        armorMod: 1,
-        weaponMod: 1.7,
-        hpMod: 2.3,
+        armorMod: 1.2,
+        weaponMod: 1.9,
+        hpMod: 2.8,
         xp: 3000,  
         respawnDelay: 30000
         
