adaee8233879cb2d8799626b2d77dbebd388a261
loopwormsbot <balkshamster@proton.me>
2023-09-18 10:02:30 +0000

    add Alpha Farms: Weapon 40, 0x1f5ac1cf4ef8853031dd669fcdaf4b1b9d94ac43bf371741d36cd626ecea7977, weapon, bot_add_nfts_2023_09_18T10_02_09

--- client/js/game.js
+++ client/js/game.js
@@ -2213,6 +2213,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_11c152fe6845dfc06d5c6f73c596423c46a39f062c57ac705d7d6676a17a1964",
                                 "NFT_1ac447f343cee09b860059149a5d022348d03d2e236d8699d180892b717f9d0c",
                                 "NFT_1aca833ec0f4116c21557ecda9fca07e2274d92175e1aaa378740e11e41c78e5",
+                                "NFT_1f5ac1cf4ef8853031dd669fcdaf4b1b9d94ac43bf371741d36cd626ecea7977",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2330,6 +2330,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_11c152fe6845dfc06d5c6f73c596423c46a39f062c57ac705d7d6676a17a1964.json',
         'text!../sprites/NFT_1ac447f343cee09b860059149a5d022348d03d2e236d8699d180892b717f9d0c.json',
         'text!../sprites/NFT_1aca833ec0f4116c21557ecda9fca07e2274d92175e1aaa378740e11e41c78e5.json',
+        'text!../sprites/NFT_1f5ac1cf4ef8853031dd669fcdaf4b1b9d94ac43bf371741d36cd626ecea7977.json',
         // @nextSpriteImport@
     ], function() {
     
