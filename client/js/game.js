353499e4b7e0732d44dad439749557a6f9c81c75
loopwormsbot <balkshamster@proton.me>
2023-09-18 10:04:30 +0000

    add Alpha Farms: Weapon 56, 0xcb45f3365acb820a6d8b56637c2abab438345b0a67dc508ffc797b0bf0e356d1, weapon, bot_add_nfts_2023_09_18T10_02_09

--- client/js/game.js
+++ client/js/game.js
@@ -2349,6 +2349,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_c85df8a89b6a10eb31e8384b2afe1ff056bbcebe89f2b5006002253be9a83cb0",
                                 "NFT_c908d40a6c9640666e49b326487979f4d7ba2da1cb973bfb97a5376ed3315f1f",
                                 "NFT_cae958559ada854cff65f82bdb36e30942762223246a4103903a60911b176234",
+                                "NFT_cb45f3365acb820a6d8b56637c2abab438345b0a67dc508ffc797b0bf0e356d1",
                                 // @nextSpriteLine@
                             ];                          
         },
