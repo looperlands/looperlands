75b45db7ed43072d91a08e199edfd0e11bae159e
loopwormsbot <balkshamster@proton.me>
2023-09-26 10:02:58 +0000

    add Iron Fists of Fury 01, 0x377c6a1d924c0f99a2edb5c7023e0125caff2f13779977afb850fbe83b262730, weapon, bot_add_nfts_2023_09_26T10_02_56

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2548,6 +2548,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_92dcfe2c15826bdf252da17c7108fe548ebf933b04088aa361fe82612610d9f4.json',
         'text!../sprites/NFT_96952da2c9e8e039898018e5c3f40d4d056bcfc411c467a9df062bb44d3de61c.json',
         'text!../sprites/NFT_d159817f241267b282609ef8af0ea9d9b978f530a9ab4a2128ba9c5825b22831.json',
+        'text!../sprites/NFT_377c6a1d924c0f99a2edb5c7023e0125caff2f13779977afb850fbe83b262730.json',
         // @nextSpriteImport@
     ], function() {
     
