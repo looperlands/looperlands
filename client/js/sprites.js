ba38797a5273125c8c21a12dd0de4117b19b8778
loopwormsbot <balkshamster@proton.me>
2023-10-01 10:04:40 +0000

    add Zharum #21, 0x6fe1de24b31e5602c5982052dc8226529019484ea899896e0d95d62799d34268, armor, bot_add_nfts_2023_10_01T10_02_31

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2702,6 +2702,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_6da6f6a30e648779b713f08b767622b5f7f33d92dd35c5da2a6407a23967f7cb.json',
         'text!../sprites/NFT_6e6f27ada1c4ffdb26d49879a39cc0aa0bc4084a68f3cf6a690e1efb010e7222.json',
         'text!../sprites/NFT_6f7e6b3cfc7272e82374256c1a0addba104e933d7a0e94ad2dba87bd7739a7ba.json',
+        'text!../sprites/NFT_6fe1de24b31e5602c5982052dc8226529019484ea899896e0d95d62799d34268.json',
         // @nextSpriteImport@
     ], function() {
     
