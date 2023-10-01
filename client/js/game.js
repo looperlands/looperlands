2c4316038f7b11dbc042e194476d48071a308f8a
loopwormsbot <balkshamster@proton.me>
2023-10-01 10:02:44 +0000

    add Rorin Kin #25, 0x0849aec5eab389a5d4f3fdd2df70c92cfacbbb339b4cc10eb1c2d5ef14bfafea, armor, bot_add_nfts_2023_10_01T10_02_31

--- client/js/game.js
+++ client/js/game.js
@@ -2467,6 +2467,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_0766eb68e7b73c027d56c870646058b248d44648398eb82dc940533bc72e94b1",
                                 "NFT_0771cc93524cd324af2772b6c0bba24a48847948c34538b14ae070792f53cc07",
                                 "NFT_07b730849ac6540ba7961617e25656479cd8c7b1ae34e1ebb37b52021c0ad655",
+                                "NFT_0849aec5eab389a5d4f3fdd2df70c92cfacbbb339b4cc10eb1c2d5ef14bfafea",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2590,6 +2590,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_0766eb68e7b73c027d56c870646058b248d44648398eb82dc940533bc72e94b1.json',
         'text!../sprites/NFT_0771cc93524cd324af2772b6c0bba24a48847948c34538b14ae070792f53cc07.json',
         'text!../sprites/NFT_07b730849ac6540ba7961617e25656479cd8c7b1ae34e1ebb37b52021c0ad655.json',
+        'text!../sprites/NFT_0849aec5eab389a5d4f3fdd2df70c92cfacbbb339b4cc10eb1c2d5ef14bfafea.json',
         // @nextSpriteImport@
     ], function() {
     
