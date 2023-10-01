1ede6c4c72b7e7786b3e570a2c3a84829eeb140d
loopwormsbot <balkshamster@proton.me>
2023-10-01 10:05:43 +0000

    add Zharum #48, 0xb166ca7cde04d9bc13396ab181de0a7e2f83d2f87766462df9e0392e69729b99, armor, bot_add_nfts_2023_10_01T10_02_31

--- client/js/game.js
+++ client/js/game.js
@@ -2634,6 +2634,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_ac719f91de6eadac7071b3f6f5df4a71507bc82dad77375c4f6a662819eb2347",
                                 "NFT_ad9bfc177354efa37ee5412fa6389d39ec5d25f10a670454034537546a8621ae",
                                 "NFT_b0af7a7b3420f0f15fb4b51d2c3118c4ddb49188c7c266e98d66b20161f4de69",
+                                "NFT_b166ca7cde04d9bc13396ab181de0a7e2f83d2f87766462df9e0392e69729b99",
                                 // @nextSpriteLine@
                             ];                          
         },
