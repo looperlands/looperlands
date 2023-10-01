4993526f5b049bb6cfd2fd9f4c3625bf7e12b39a
loopwormsbot <balkshamster@proton.me>
2023-10-01 10:04:13 +0000

    add Beta looper PTTA, 0x5dab57e506f3132065d13c379d953aae89f2b15b75e1d753584b68ebc817ee13, armor, bot_add_nfts_2023_10_01T10_02_31

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2675,6 +2675,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_5af1e35ef33601be4438b6e74b239ee910d72af8908c24d3293cf67f6408f5b6.json',
         'text!../sprites/NFT_5b88bf62994cddbd143fd754f0b5684bc0f2101414338930d8debfc30f117c2e.json',
         'text!../sprites/NFT_5c232123af7da6e2bf0581c06ba1226cd384ea3e718d5914815c6e6d4a45321e.json',
+        'text!../sprites/NFT_5dab57e506f3132065d13c379d953aae89f2b15b75e1d753584b68ebc817ee13.json',
         // @nextSpriteImport@
     ], function() {
     
