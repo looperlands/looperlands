dbbd35f912ce22e6df55fe1243e826722f698daa
loopwormsbot <balkshamster@proton.me>
2023-10-01 10:04:09 +0000

    add Zharum #37, 0x5acc9fcaa8dd5ac39d4cc30fda8729c23e32b7a7b08d260b4a635d79aa273d56, armor, bot_add_nfts_2023_10_01T10_02_31

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -2698,7 +2698,8 @@ Types = {
         NFT_55da7e4aec91c3b01e9da06c9af85d36fffa78ac74709aefec7d125973543dfe: 2497,
         NFT_55dd137bc0251068ebc5cea09663a9c295c8a2693f3c24e5d92bb36a7e7f7af6: 2498,
         NFT_5786db70eb36383d1176c34ee178a0da0d0029b8adb54918bd8d43480312773c: 2499,
-        NFT_5a7dca50a661709cec9a2b4cdc00b38f3b0a4231c1afa95abef983ce181d0131: 2500 // @lastid@
+        NFT_5a7dca50a661709cec9a2b4cdc00b38f3b0a4231c1afa95abef983ce181d0131: 2500,
+        NFT_5acc9fcaa8dd5ac39d4cc30fda8729c23e32b7a7b08d260b4a635d79aa273d56: 2501 // @lastid@
         //@nextIdLine@
     },
     
@@ -5393,6 +5394,7 @@ var kinds = {
     NFT_55dd137bc0251068ebc5cea09663a9c295c8a2693f3c24e5d92bb36a7e7f7af6: [Types.Entities.NFT_55dd137bc0251068ebc5cea09663a9c295c8a2693f3c24e5d92bb36a7e7f7af6, "armor"],
     NFT_5786db70eb36383d1176c34ee178a0da0d0029b8adb54918bd8d43480312773c: [Types.Entities.NFT_5786db70eb36383d1176c34ee178a0da0d0029b8adb54918bd8d43480312773c, "armor"],
     NFT_5a7dca50a661709cec9a2b4cdc00b38f3b0a4231c1afa95abef983ce181d0131: [Types.Entities.NFT_5a7dca50a661709cec9a2b4cdc00b38f3b0a4231c1afa95abef983ce181d0131, "armor"],
+    NFT_5acc9fcaa8dd5ac39d4cc30fda8729c23e32b7a7b08d260b4a635d79aa273d56: [Types.Entities.NFT_5acc9fcaa8dd5ac39d4cc30fda8729c23e32b7a7b08d260b4a635d79aa273d56, "armor"],
     // @nextSpriteLine@
 
     
