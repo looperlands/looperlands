823d5a0f910725c1a07613a1aa5af021a94bad41
loopwormsbot <balkshamster@proton.me>
2023-10-04 10:03:07 +0000

    add Lil Julionut, 0xf62c22d4577ae5812cf256375682435d992ffe6e937c2491085f7076ec0c42b3, armor, bot_add_nfts_2023_10_04T10_03_00

--- client/js/game.js
+++ client/js/game.js
@@ -2736,6 +2736,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_94ddcdecb3034d5a1f10d035d9ab172f67943e943016cfec2532eba2f248c5fe",
                                 "NFT_a96c1e78ca90d8c6d1fa72c5cd7dd679a4dc3a5fa0d04bfc78fcb2f2d53b7c4e",
                                 "NFT_01348c49000000000000000002386f26fc100000000000000000000000000394",
+                                "NFT_f62c22d4577ae5812cf256375682435d992ffe6e937c2491085f7076ec0c42b3",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2868,6 +2868,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_94ddcdecb3034d5a1f10d035d9ab172f67943e943016cfec2532eba2f248c5fe.json',
         'text!../sprites/NFT_a96c1e78ca90d8c6d1fa72c5cd7dd679a4dc3a5fa0d04bfc78fcb2f2d53b7c4e.json',
         'text!../sprites/NFT_01348c49000000000000000002386f26fc100000000000000000000000000394.json',
+        'text!../sprites/NFT_f62c22d4577ae5812cf256375682435d992ffe6e937c2491085f7076ec0c42b3.json',
         // @nextSpriteImport@
     ], function() {
     
