51aa321a0420b20b5c355be3b66c51a8bdd3c528
loopwormsbot <balkshamster@proton.me>
2023-10-10 10:03:17 +0000

    add PhurntManDimmiez 284, 0x56698e533e8ce68eef71eb9c448412bbf2fcea4bd1665bdedaec82256f3b539f, armor, bot_add_nfts_2023_10_10T10_03_08

--- client/js/game.js
+++ client/js/game.js
@@ -2746,6 +2746,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_298bedfa24390c4f2d232cb70278a310bfeb8296270fc8484f374a02c29589af",
                                 "NFT_2bb4c4ad46e385885ea7992f92fe63caf7e5d3d9a80ae1b2a4e9ac28510605aa",
                                 "NFT_3f7f3db792aac799d656e5b89ddd3e459f330a731174d27a8740845dcec35052",
+                                "NFT_56698e533e8ce68eef71eb9c448412bbf2fcea4bd1665bdedaec82256f3b539f",
                                 // @nextSpriteLine@
                             ];                          
         },
