89adc498acaf67ae58519b3d3020412882ee7f03
loopwormsbot <balkshamster@proton.me>
2023-10-13 10:05:16 +0000

    add Red Dragon Slayer 51, 0xa646d75e6949ce531df537876ef51edf2e844bf1098d3e420ec2d29eecb3783e, armor, bot_add_nfts_2023_10_13T10_02_43

--- client/js/game.js
+++ client/js/game.js
@@ -2879,6 +2879,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_a2e151249aef0ec8398485b0d0564ffcb0fec65e50382aab5e4d67b6f17e90cf",
                                 "NFT_a515fcdd846d5efe65e7df9bbc491773a237acb471f33b5068c4b41eea156ebb",
                                 "NFT_a60798687857a7a6567c4420284e96726ac5a69af384f4a57038b808959e4a6d",
+                                "NFT_a646d75e6949ce531df537876ef51edf2e844bf1098d3e420ec2d29eecb3783e",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -3011,6 +3011,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_a2e151249aef0ec8398485b0d0564ffcb0fec65e50382aab5e4d67b6f17e90cf.json',
         'text!../sprites/NFT_a515fcdd846d5efe65e7df9bbc491773a237acb471f33b5068c4b41eea156ebb.json',
         'text!../sprites/NFT_a60798687857a7a6567c4420284e96726ac5a69af384f4a57038b808959e4a6d.json',
+        'text!../sprites/NFT_a646d75e6949ce531df537876ef51edf2e844bf1098d3e420ec2d29eecb3783e.json',
         // @nextSpriteImport@
     ], function() {
     
