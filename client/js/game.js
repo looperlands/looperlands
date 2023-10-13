76b0897a0ac53ec0ce93f4b98fb35c02abe3e04f
loopwormsbot <balkshamster@proton.me>
2023-10-13 10:04:00 +0000

    add Red Dragon Slayer 79, 0x577c2629353ca293543019bb0ebae171fc64b37f8dd886432e0433c7b8a0cdf8, armor, bot_add_nfts_2023_10_13T10_02_43

--- client/js/game.js
+++ client/js/game.js
@@ -2817,6 +2817,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_55f12ec737971c021a766e70870961309a219757c191b7d8ed321c3aa67d6d9d",
                                 "NFT_565a6b9a00da314351ae7a81a6c9a7bc6bc51bfb198ad92b239c9f9df8a6ba7a",
                                 "NFT_56ba78be2d9c9d58fb20568f73b87e0100845b8efec41797f187f40d4b27ec49",
+                                "NFT_577c2629353ca293543019bb0ebae171fc64b37f8dd886432e0433c7b8a0cdf8",
                                 // @nextSpriteLine@
                             ];                          
         },
