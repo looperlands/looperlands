2a4c0b4ff93f7cd31d961a1b6bfc4f1efcdd032e
loopwormsbot <balkshamster@proton.me>
2023-10-13 10:04:28 +0000

    add Red Panda 14, 0x7a4f23c6189996d2b00e409e75d032063893cd1fb4a13d1448e0df08e9739875, armor, bot_add_nfts_2023_10_13T10_02_43

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -2974,6 +2974,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_741a34e366ca820df9fa697d1bbf2b777d76346a7cc79edcd91959732458a6a9.json',
         'text!../sprites/NFT_76d88fa8597ed85d3965998f056543e5752fd8ca1e7115201e7a50dbcb77c078.json',
         'text!../sprites/NFT_789fa6e3d21ff75048798d81a4646c95784e2d0f3f26af32d22715fe8001f294.json',
+        'text!../sprites/NFT_7a4f23c6189996d2b00e409e75d032063893cd1fb4a13d1448e0df08e9739875.json',
         // @nextSpriteImport@
     ], function() {
     
