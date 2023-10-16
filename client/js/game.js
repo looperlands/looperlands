bf277d58cb0ecaefb075387303b62ae0a441786d
loopwormsbot <balkshamster@proton.me>
2023-10-16 10:03:07 +0000

    add PhurntGhostDimmie 004, 0x1766de568a9bd49d06cd09c482187ab17ff3264a6ddb6526369014620ba969e5, armor, bot_add_nfts_2023_10_16T10_03_00

--- client/js/game.js
+++ client/js/game.js
@@ -2976,6 +2976,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_73719be37a30a16ab5afb4d568432a908996171bb2f3306f72b32765b9100287",
                                 "NFT_bc21417dc528e2632d7de7e99d07284ebbc76d58f2e13a8e8b860ee2457dde20",
                                 "NFT_cfdcbf94adff135c20e05c21198b5657a44a6b4226d5a223789da88af4d9a05b",
+                                "NFT_1766de568a9bd49d06cd09c482187ab17ff3264a6ddb6526369014620ba969e5",
                                 // @nextSpriteLine@
                             ];                          
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -3108,6 +3108,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_73719be37a30a16ab5afb4d568432a908996171bb2f3306f72b32765b9100287.json',
         'text!../sprites/NFT_bc21417dc528e2632d7de7e99d07284ebbc76d58f2e13a8e8b860ee2457dde20.json',
         'text!../sprites/NFT_cfdcbf94adff135c20e05c21198b5657a44a6b4226d5a223789da88af4d9a05b.json',
+        'text!../sprites/NFT_1766de568a9bd49d06cd09c482187ab17ff3264a6ddb6526369014620ba969e5.json',
         // @nextSpriteImport@
     ], function() {
     
