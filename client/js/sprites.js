90d7ccbfbfd77d8f622c021cc59261db278d0c2f
loopwormsbot <balkshamster@proton.me>
2023-10-22 10:03:15 +0000

    add DATAdude 076, 0x0c4bcd8384eb657198d3a494f5debbc792006e9da39043e6637ea44c4c21622a, armor, bot_add_nfts_2023_10_22T10_03_12

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -3145,6 +3145,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_2277d811bc49b3e39c2121b0f06f200fadb76320ceeec07894d03168416d14be.json',
         'text!../sprites/NFT_5d210eaa23437df3a70e91815f37ae652c10c4db5c5e687249efca48d062e400.json',
         'text!../sprites/NFT_f6526224a4fb72a237fe3f87724abfe3846827aa03982ac1dfe48af3d03b6e2e.json',
+        'text!../sprites/NFT_0c4bcd8384eb657198d3a494f5debbc792006e9da39043e6637ea44c4c21622a.json',
         // @nextSpriteImport@
     ], function() {
     
