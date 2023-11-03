21c7859ee1e75c8f8a30cdbe73ddd1fe8e7d7afc
CyberPanel <support@cyberpanel.net>
2023-11-03 10:04:00 +0000

    add DATAdude 020, 0x63b34e07898768060005facb70192ee0be7778623bcc45ff332e3ab40582c972, armor, bot_add_nfts_2023_11_03T10_03_20

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -3455,6 +3455,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_2a23fdae818b5717fc30f733b105d027a063dbf3b3e1696054029373f0775490.json',
         'text!../sprites/NFT_39b995a32319c47082555363a2d47245bf486740bcf84b7a4fb8d5e9db548f02.json',
         'text!../sprites/NFT_3e47bb4df08b0ab6123c32509cbd2d9087dc01cd197a22d8a23f7b826ecfba7e.json',
+        'text!../sprites/NFT_63b34e07898768060005facb70192ee0be7778623bcc45ff332e3ab40582c972.json',
         // @nextSpriteImport@
     ], function() {
     
