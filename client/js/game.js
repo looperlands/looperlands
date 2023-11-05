13fd031336550b70438754979a00bbef9ec7507b
CyberPanel <support@cyberpanel.net>
2023-11-05 10:03:35 +0000

    add LW | Prisoner Worm, 0xc52790a81539d618f0cd3c401f6798a817a870b16c608ff7326bd929cdf4c71b, armor, bot_add_nfts_2023_11_05T10_03_28

--- client/js/game.js
+++ client/js/game.js
@@ -3334,6 +3334,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_5fde4a1293ca5c424b52d18687c2513955f2c534dbf05e7d2cd2c30f9913de71",
                                 "NFT_819fd15a9b14605c37f1633098e464a5e9453d370217f8c22860ebb14a0f150d",
                                 "NFT_a3a6f62a6950717e4d4a905540e048741f00ea02378c624437cb68a5c4b95b64",
+                                "NFT_c52790a81539d618f0cd3c401f6798a817a870b16c608ff7326bd929cdf4c71b",
                                 // @nextSpriteLine@
                             ];
         },
