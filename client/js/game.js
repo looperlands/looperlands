c56c96bdde6bc43490d1df8898644601fc3d8a75
CyberPanel <support@cyberpanel.net>
2023-11-08 12:21:41 +0000

    add Tendie Hammer 72, 0x8cb1a738a96347a58b33fdd0ce4e80e27595b567a2ed661c408f4cf9109f44d4, weapon, bot_add_nfts_2023_11_08T12_20_27

--- client/js/game.js
+++ client/js/game.js
@@ -3418,6 +3418,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_8b7fec8240c1f8cf4087efc2812631086a703a78f921029618cd947e6ada5611",
                                 "NFT_8b85c633f843514baf6a07f6172f2953d3afc777c659b6e3aa0caa753a407ed9",
                                 "NFT_8c2d8d17ba08f9312bdb61e9802a28b9afb54f86e2f78a68dfb3c5509bbf3451",
+                                "NFT_8cb1a738a96347a58b33fdd0ce4e80e27595b567a2ed661c408f4cf9109f44d4",
                                 // @nextSpriteLine@
                             ];
         },
--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -3495,7 +3495,8 @@ Types = {
         NFT_8ab17f69eb065c1cfbe3c0120a7e516195b3a0b7a7d621eb6127ea1bb23b1742: 3278,
         NFT_8b7fec8240c1f8cf4087efc2812631086a703a78f921029618cd947e6ada5611: 3279,
         NFT_8b85c633f843514baf6a07f6172f2953d3afc777c659b6e3aa0caa753a407ed9: 3280,
-        NFT_8c2d8d17ba08f9312bdb61e9802a28b9afb54f86e2f78a68dfb3c5509bbf3451: 3281 // @lastid@
+        NFT_8c2d8d17ba08f9312bdb61e9802a28b9afb54f86e2f78a68dfb3c5509bbf3451: 3281,
+        NFT_8cb1a738a96347a58b33fdd0ce4e80e27595b567a2ed661c408f4cf9109f44d4: 3282 // @lastid@
         //@nextIdLine@
     },
     
@@ -6984,6 +6985,7 @@ var kinds = {
     NFT_8b7fec8240c1f8cf4087efc2812631086a703a78f921029618cd947e6ada5611: [Types.Entities.NFT_8b7fec8240c1f8cf4087efc2812631086a703a78f921029618cd947e6ada5611, "weapon"],
     NFT_8b85c633f843514baf6a07f6172f2953d3afc777c659b6e3aa0caa753a407ed9: [Types.Entities.NFT_8b85c633f843514baf6a07f6172f2953d3afc777c659b6e3aa0caa753a407ed9, "weapon"],
     NFT_8c2d8d17ba08f9312bdb61e9802a28b9afb54f86e2f78a68dfb3c5509bbf3451: [Types.Entities.NFT_8c2d8d17ba08f9312bdb61e9802a28b9afb54f86e2f78a68dfb3c5509bbf3451, "weapon"],
+    NFT_8cb1a738a96347a58b33fdd0ce4e80e27595b567a2ed661c408f4cf9109f44d4: [Types.Entities.NFT_8cb1a738a96347a58b33fdd0ce4e80e27595b567a2ed661c408f4cf9109f44d4, "weapon"],
     // @nextSpriteLine@
 
     
