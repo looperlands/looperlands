1769faa2a2beca5c4dc75e69084b7dc7f794848c
CyberPanel <support@cyberpanel.net>
2023-11-08 12:21:39 +0000

    add Tendie Hammer 12, 0x8b85c633f843514baf6a07f6172f2953d3afc777c659b6e3aa0caa753a407ed9, weapon, bot_add_nfts_2023_11_08T12_20_27

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -3556,6 +3556,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_891c71eda7dd61259fdb6ea914cd8d1cf142b66825fd1901b68e5021e82d72e1.json',
         'text!../sprites/NFT_8ab17f69eb065c1cfbe3c0120a7e516195b3a0b7a7d621eb6127ea1bb23b1742.json',
         'text!../sprites/NFT_8b7fec8240c1f8cf4087efc2812631086a703a78f921029618cd947e6ada5611.json',
+        'text!../sprites/NFT_8b85c633f843514baf6a07f6172f2953d3afc777c659b6e3aa0caa753a407ed9.json',
         // @nextSpriteImport@
     ], function() {
     
