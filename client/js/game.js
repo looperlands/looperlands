50034546a543f78daf5000245b3a0230dfbf78d7
CyberPanel <support@cyberpanel.net>
2023-11-09 10:04:04 +0000

    add PXL NANCY 858, 0x83376e8e30bc3d94da9cd7b40e98a2dc3a512f1a25b9300e577f94b84b05bc72, armor, bot_add_nfts_2023_11_09T10_02_56

--- client/js/game.js
+++ client/js/game.js
@@ -3486,6 +3486,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_64d7e7f0cef76a754845fc17ce9ea0746c6e4a4e75ce2a04b7613898ed05775f",
                                 "NFT_66c595042171a17d84b7e690d886558ebc907b29656fad29dbfb696a4f06286e",
                                 "NFT_81356e280eb689c38cfc77ab537e9752f612450c18da62fff636ed1a352fa73b",
+                                "NFT_83376e8e30bc3d94da9cd7b40e98a2dc3a512f1a25b9300e577f94b84b05bc72",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -3626,6 +3626,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_64d7e7f0cef76a754845fc17ce9ea0746c6e4a4e75ce2a04b7613898ed05775f.json',
         'text!../sprites/NFT_66c595042171a17d84b7e690d886558ebc907b29656fad29dbfb696a4f06286e.json',
         'text!../sprites/NFT_81356e280eb689c38cfc77ab537e9752f612450c18da62fff636ed1a352fa73b.json',
+        'text!../sprites/NFT_83376e8e30bc3d94da9cd7b40e98a2dc3a512f1a25b9300e577f94b84b05bc72.json',
         // @nextSpriteImport@
     ], function() {
     
