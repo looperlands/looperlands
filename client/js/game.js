056d9e9d6eefea27268948496b5c0fe562a80f25
CyberPanel <support@cyberpanel.net>
2023-11-26 10:06:29 +0000

    add Aeros, 0xfe674b8ac9437a69bf444fc9aa7070557a7e1eb4ca4084496fac04a825802229, armor, bot_add_nfts_2023_11_26T10_03_02

--- client/js/game.js
+++ client/js/game.js
@@ -3862,6 +3862,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_fa1260cb2847e90f420bb5c3f6a2c26d486f0afafe05f72532eb4a9e64d7f5b2",
                                 "NFT_fa3e53a69eedf9b0a50e91e1b3301ae0b23404e833ff0a666fdf6c5689c23b1a",
                                 "NFT_faf59792629c23eb446886f2efa157b7bfa44a76482c0acd4bf412d1aa752a51",
+                                "NFT_fe674b8ac9437a69bf444fc9aa7070557a7e1eb4ca4084496fac04a825802229",
                                 // @nextSpriteLine@
                             ];
         },
--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -3927,7 +3927,8 @@ Types = {
         NFT_f8bad3a26902dd1c67eacc259756091b9b4b393629c68a46993a980d5dfb39ca: 3699,
         NFT_fa1260cb2847e90f420bb5c3f6a2c26d486f0afafe05f72532eb4a9e64d7f5b2: 3700,
         NFT_fa3e53a69eedf9b0a50e91e1b3301ae0b23404e833ff0a666fdf6c5689c23b1a: 3701,
-        NFT_faf59792629c23eb446886f2efa157b7bfa44a76482c0acd4bf412d1aa752a51: 3702 // @lastid@
+        NFT_faf59792629c23eb446886f2efa157b7bfa44a76482c0acd4bf412d1aa752a51: 3702,
+        NFT_fe674b8ac9437a69bf444fc9aa7070557a7e1eb4ca4084496fac04a825802229: 3703 // @lastid@
         //@nextIdLine@
     },
     
@@ -7839,6 +7840,7 @@ var kinds = {
     NFT_fa1260cb2847e90f420bb5c3f6a2c26d486f0afafe05f72532eb4a9e64d7f5b2: [Types.Entities.NFT_fa1260cb2847e90f420bb5c3f6a2c26d486f0afafe05f72532eb4a9e64d7f5b2, "armor"],
     NFT_fa3e53a69eedf9b0a50e91e1b3301ae0b23404e833ff0a666fdf6c5689c23b1a: [Types.Entities.NFT_fa3e53a69eedf9b0a50e91e1b3301ae0b23404e833ff0a666fdf6c5689c23b1a, "armor"],
     NFT_faf59792629c23eb446886f2efa157b7bfa44a76482c0acd4bf412d1aa752a51: [Types.Entities.NFT_faf59792629c23eb446886f2efa157b7bfa44a76482c0acd4bf412d1aa752a51, "armor"],
+    NFT_fe674b8ac9437a69bf444fc9aa7070557a7e1eb4ca4084496fac04a825802229: [Types.Entities.NFT_fe674b8ac9437a69bf444fc9aa7070557a7e1eb4ca4084496fac04a825802229, "armor"],
     // @nextSpriteLine@
 
     
