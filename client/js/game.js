a42c03115b064a29116ff7f9e6fa3f887f9ed99d
CyberPanel <support@cyberpanel.net>
2023-11-26 10:03:35 +0000

    add Athrage, 0x1b52492ff89f86c91bd1c343d013526a3b29540e384feaa4e176346b60e12770, armor, bot_add_nfts_2023_11_26T10_03_02

--- client/js/game.js
+++ client/js/game.js
@@ -3714,6 +3714,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_17997e6af733fc220d2a862d04a9e330d11d281865058f18862c48f64d3e0296",
                                 "NFT_18cc127f230b1b027cd8944005adb8b73b0a70f4c72f0a15edf1ee11d05b0360",
                                 "NFT_1b1273ea922d460b2ff8ca4a11faec2f96bcb01d480d35b1a286c94e9c0457e6",
+                                "NFT_1b52492ff89f86c91bd1c343d013526a3b29540e384feaa4e176346b60e12770",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -3852,6 +3852,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_17997e6af733fc220d2a862d04a9e330d11d281865058f18862c48f64d3e0296.json',
         'text!../sprites/NFT_18cc127f230b1b027cd8944005adb8b73b0a70f4c72f0a15edf1ee11d05b0360.json',
         'text!../sprites/NFT_1b1273ea922d460b2ff8ca4a11faec2f96bcb01d480d35b1a286c94e9c0457e6.json',
+        'text!../sprites/NFT_1b52492ff89f86c91bd1c343d013526a3b29540e384feaa4e176346b60e12770.json',
         // @nextSpriteImport@
     ], function() {
     
