1fec1308ea092983c4c2a129081b0219d571dfe2
CyberPanel <support@cyberpanel.net>
2023-11-26 10:03:52 +0000

    add Crogo, 0x2cfa804a0644c74e3e12bba193e4bf75230ce4f0948b2832acf92f44852f6805, armor, bot_add_nfts_2023_11_26T10_03_02

--- client/js/game.js
+++ client/js/game.js
@@ -3728,6 +3728,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_2847fa4c83d0e93010ab2844df1fc57327d9e8c546155a17f16de6e26143ed3c",
                                 "NFT_2a80ae90eb6e662276cc0d206d8eca9487f497e452cb7e67ba2bcff39af2c6dd",
                                 "NFT_2ceae9bc05a37c29bb01eaae1329f07886dc5d6ffd9f53670b5c99b241eea54c",
+                                "NFT_2cfa804a0644c74e3e12bba193e4bf75230ce4f0948b2832acf92f44852f6805",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -3866,6 +3866,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_2847fa4c83d0e93010ab2844df1fc57327d9e8c546155a17f16de6e26143ed3c.json',
         'text!../sprites/NFT_2a80ae90eb6e662276cc0d206d8eca9487f497e452cb7e67ba2bcff39af2c6dd.json',
         'text!../sprites/NFT_2ceae9bc05a37c29bb01eaae1329f07886dc5d6ffd9f53670b5c99b241eea54c.json',
+        'text!../sprites/NFT_2cfa804a0644c74e3e12bba193e4bf75230ce4f0948b2832acf92f44852f6805.json',
         // @nextSpriteImport@
     ], function() {
     
