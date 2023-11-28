c2696102191831b30e28f81f67ab06fdf5ae828e
CyberPanel <support@cyberpanel.net>
2023-11-28 10:04:25 +0000

    add Observer 4, 0x012498e5bead24a3dea7c0d64135b2ba79b69aec58dc70182449228b124e7819, armor, bot_add_nfts_2023_11_28T10_04_22

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4003,6 +4003,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_fe674b8ac9437a69bf444fc9aa7070557a7e1eb4ca4084496fac04a825802229.json',
         'text!../sprites/NFT_fedbb791db1dd371b7e21d39d1ee970112e3f52ca94496bc6b0a927d2e29c4d4.json',
         'text!../sprites/NFT_ff84914ea8ecbed34d510d3bda332fa2c0ba7b101aea910dcb6b2d566c498b34.json',
+        'text!../sprites/NFT_012498e5bead24a3dea7c0d64135b2ba79b69aec58dc70182449228b124e7819.json',
         // @nextSpriteImport@
     ], function() {
     
