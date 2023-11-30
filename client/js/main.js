17c35fbe73608f8d5282eef8f0abd5a27f4d3cef
Andre de Groot <>
2023-11-30 23:10:17 +0100

    fullscreen

--- client/js/main.js
+++ client/js/main.js
@@ -360,6 +360,11 @@ define(['jquery', 'app'], function($, App) {
                 });
             }
 
+            // Zoom game area to fill 95% of window size.
+            window.onresize = () => $("#container").css('transform', 'scale(' + Math.min(($(window).width() / $("#container").width() * 0.95), ($(window).height() / $("#container").height() * 0.95)) + ')');
+            $("#container").css('transform', 'scale(' + Math.min(($(window).width() / $("#container").width() * 0.95), ($(window).height() / $("#container").height() * 0.95)) + ')')
+
+
             $('body').unbind('click');
             $('body').click(function(event) {
                 var hasClosedParchment = false;
