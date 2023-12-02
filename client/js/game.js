ccfeeeb6b8b5b20a0e75bca27d619621699522b4
CyberPanel <support@cyberpanel.net>
2023-12-02 14:27:05 +0000

    add Madness Of Snow, 0xa7213d3da60af52fc9fbb546f572c05419107c599f9a4980b905138dccbd79ae, armor, bot_add_nfts_2023_12_02T14_26_43

--- client/js/game.js
+++ client/js/game.js
@@ -3885,6 +3885,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_631748c023c7bf5d146e63b35bd44679404b93910b195f7e48f931865506db5d",
                                 "NFT_99b7e8ae13738b9093d6721819a988d395bcf0ea1a1900a665532b31317c97c9",
                                 "NFT_a62d46905299c7ee1c50a19b9355fe93157cdbfc4e480c788c5c587df4a99a96",
+                                "NFT_a7213d3da60af52fc9fbb546f572c05419107c599f9a4980b905138dccbd79ae",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4024,6 +4024,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_631748c023c7bf5d146e63b35bd44679404b93910b195f7e48f931865506db5d.json',
         'text!../sprites/NFT_99b7e8ae13738b9093d6721819a988d395bcf0ea1a1900a665532b31317c97c9.json',
         'text!../sprites/NFT_a62d46905299c7ee1c50a19b9355fe93157cdbfc4e480c788c5c587df4a99a96.json',
+        'text!../sprites/NFT_a7213d3da60af52fc9fbb546f572c05419107c599f9a4980b905138dccbd79ae.json',
         // @nextSpriteImport@
     ], function() {
     
