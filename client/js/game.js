e59c2bf5966d0d562bf2557f5c95b56eb4fb3546
CyberPanel <support@cyberpanel.net>
2023-12-02 14:27:06 +0000

    add hamster venom, 0xaa2b297780278493a0c09ed4e1794569b96b0a35f267369384026b653914c2fd, weapon, bot_add_nfts_2023_12_02T14_26_43

--- client/js/game.js
+++ client/js/game.js
@@ -3886,6 +3886,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_99b7e8ae13738b9093d6721819a988d395bcf0ea1a1900a665532b31317c97c9",
                                 "NFT_a62d46905299c7ee1c50a19b9355fe93157cdbfc4e480c788c5c587df4a99a96",
                                 "NFT_a7213d3da60af52fc9fbb546f572c05419107c599f9a4980b905138dccbd79ae",
+                                "NFT_aa2b297780278493a0c09ed4e1794569b96b0a35f267369384026b653914c2fd",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4025,6 +4025,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_99b7e8ae13738b9093d6721819a988d395bcf0ea1a1900a665532b31317c97c9.json',
         'text!../sprites/NFT_a62d46905299c7ee1c50a19b9355fe93157cdbfc4e480c788c5c587df4a99a96.json',
         'text!../sprites/NFT_a7213d3da60af52fc9fbb546f572c05419107c599f9a4980b905138dccbd79ae.json',
+        'text!../sprites/NFT_aa2b297780278493a0c09ed4e1794569b96b0a35f267369384026b653914c2fd.json',
         // @nextSpriteImport@
     ], function() {
     
