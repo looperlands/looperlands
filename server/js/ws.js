63c12e9d5cf99af2613b78a7209af1e090762962
balkshamster <128100383+balkshamster@users.noreply.github.com>
2023-12-09 09:45:48 -0500

    Sync the Exp properly (#672)

--- server/js/ws.js
+++ server/js/ws.js
@@ -165,6 +165,7 @@ WS.socketIOServer = Server.extend({
                 let ownYourLoopersBuff = 25000 * total;
                 //console.log("Asset count: ", total,  " for wallet " + playerCache.walletId + " and nft " + playerCache.nftId);
                 body.xp = parseInt(body.xp) + ownYourLoopersBuff;
+                body.ownYourLoopersBuff = ownYourLoopersBuff;
             }
 
             cache.set(id, body);
