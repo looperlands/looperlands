84eee060a29c0a0d1c2d4f5ac6de98fb6af32626
CyberPanel <support@cyberpanel.net>
2023-12-11 10:04:55 +0000

    add JingleCombatHat, 0x99fdf6a0459adbc4066d14dc2d6258bcb240dad833e0aa18d6f0d45001eb38e2, weapon, bot_add_nfts_2023_12_11T10_03_34

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -4059,7 +4059,8 @@ Types = {
         NFT_8de1257976353e89ab55081c4a8bd17e55bd5c4ce1c9205e2be8e4cfd6cd4257: 3829,
         NFT_8ea5b3bd2e985c9397435c2aa8495821da5a60fa96d4652001bcaad732805650: 3830,
         NFT_9217b2ab78f0aec66cb680d1fef692cd29bc246b051598fd6400103aa6ae7a91: 3831,
-        NFT_9258a28be74350bcbfd2f41d969527ffb1eaeb5a5d136fb556d0717df0e1562e: 3832 // @lastid@
+        NFT_9258a28be74350bcbfd2f41d969527ffb1eaeb5a5d136fb556d0717df0e1562e: 3832,
+        NFT_99fdf6a0459adbc4066d14dc2d6258bcb240dad833e0aa18d6f0d45001eb38e2: 3833 // @lastid@
         //@nextIdLine@
     },
     
@@ -8102,6 +8103,7 @@ var kinds = {
     NFT_8ea5b3bd2e985c9397435c2aa8495821da5a60fa96d4652001bcaad732805650: [Types.Entities.NFT_8ea5b3bd2e985c9397435c2aa8495821da5a60fa96d4652001bcaad732805650, "armor"],
     NFT_9217b2ab78f0aec66cb680d1fef692cd29bc246b051598fd6400103aa6ae7a91: [Types.Entities.NFT_9217b2ab78f0aec66cb680d1fef692cd29bc246b051598fd6400103aa6ae7a91, "armor"],
     NFT_9258a28be74350bcbfd2f41d969527ffb1eaeb5a5d136fb556d0717df0e1562e: [Types.Entities.NFT_9258a28be74350bcbfd2f41d969527ffb1eaeb5a5d136fb556d0717df0e1562e, "armor"],
+    NFT_99fdf6a0459adbc4066d14dc2d6258bcb240dad833e0aa18d6f0d45001eb38e2: [Types.Entities.NFT_99fdf6a0459adbc4066d14dc2d6258bcb240dad833e0aa18d6f0d45001eb38e2, "weapon"],
     // @nextSpriteLine@
 
     
