5655c1c9b17932a6123f224b5e770a55a94f7ad6
CyberPanel <support@cyberpanel.net>
2023-12-12 10:03:55 +0000

    add Amethyst Sawtooth Caster, 0x30c5b9bca4013eefebb96e28929b5f83f514eba5a060f2f9d637cb12f0705051, fishingrod, bot_add_nfts_2023_12_12T10_02_59

--- client/js/game.js
+++ client/js/game.js
@@ -4087,6 +4087,8 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "item-NFT_2e26f67e1b37c1cb7d9537e19a0b3c7365c989587943a731dfca9714543857fa",
                                 "NFT_2f721451f11d52f6f5f86569c743dd4cd8eea02a805ae968a8719f4bc8397651",
                                 "item-NFT_2f721451f11d52f6f5f86569c743dd4cd8eea02a805ae968a8719f4bc8397651",
+                                "NFT_30c5b9bca4013eefebb96e28929b5f83f514eba5a060f2f9d637cb12f0705051",
+                                "item-NFT_30c5b9bca4013eefebb96e28929b5f83f514eba5a060f2f9d637cb12f0705051",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4226,6 +4226,8 @@ define(['text!../sprites/agent.json',
         'text!../sprites/item-NFT_2e26f67e1b37c1cb7d9537e19a0b3c7365c989587943a731dfca9714543857fa.json',
         'text!../sprites/NFT_2f721451f11d52f6f5f86569c743dd4cd8eea02a805ae968a8719f4bc8397651.json',
         'text!../sprites/item-NFT_2f721451f11d52f6f5f86569c743dd4cd8eea02a805ae968a8719f4bc8397651.json',
+        'text!../sprites/NFT_30c5b9bca4013eefebb96e28929b5f83f514eba5a060f2f9d637cb12f0705051.json',
+        'text!../sprites/item-NFT_30c5b9bca4013eefebb96e28929b5f83f514eba5a060f2f9d637cb12f0705051.json',
         // @nextSpriteImport@
     ], function() {
     
