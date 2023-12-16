008ab9216fabf21799643f8b0f9b3ea292965b94
CyberPanel <support@cyberpanel.net>
2023-12-16 10:03:27 +0000

    add Lynn, 0xbdfe7bc1143b63a3ebd00147b61653efd9bc359ee0bbdd4a3cc8ea741ac826c4, armor, bot_add_nfts_2023_12_16T10_03_01

--- client/js/game.js
+++ client/js/game.js
@@ -4455,6 +4455,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_5d8e3d3d2b279acf03fa50b798ab672f8224f720a4cc71fdda1dd7ba4cc6a971",
                                 "NFT_b69a58c9f2a4e96067f0eb14087bcef9a772e5a396da6c1ceaff3ab51f5d8d17",
                                 "NFT_bc777de8bca3f57847ba4696e1a6b296a414e592377e1981fc985224324ab285",
+                                "NFT_bdfe7bc1143b63a3ebd00147b61653efd9bc359ee0bbdd4a3cc8ea741ac826c4",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4595,6 +4595,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_5d8e3d3d2b279acf03fa50b798ab672f8224f720a4cc71fdda1dd7ba4cc6a971.json',
         'text!../sprites/NFT_b69a58c9f2a4e96067f0eb14087bcef9a772e5a396da6c1ceaff3ab51f5d8d17.json',
         'text!../sprites/NFT_bc777de8bca3f57847ba4696e1a6b296a414e592377e1981fc985224324ab285.json',
+        'text!../sprites/NFT_bdfe7bc1143b63a3ebd00147b61653efd9bc359ee0bbdd4a3cc8ea741ac826c4.json',
         // @nextSpriteImport@
     ], function() {
     
