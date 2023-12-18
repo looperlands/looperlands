d5c0c75e8d78e5a98cd56c4bd33a50eb35675cf6
balkshamster <128100383+balkshamster@users.noreply.github.com>
2023-12-18 11:06:37 -0500

    check for undefined before checking if it is a bot (#719)

--- server/js/ws.js
+++ server/js/ws.js
@@ -704,7 +704,7 @@ WS.socketIOServer = Server.extend({
                 let key = cacheKeys[i];
                 let cachedBody = cache.get(key);
                 let player = self.worldsMap[cachedBody.mapId]?.getPlayerById(cachedBody.entityId);
-                if(!player.isBot() && cachedBody.isDirty === true && player !== undefined) {
+                if(cachedBody.isDirty === true && player !== undefined && !player.isBot()) {
                     let player = {
                         name: await ens.getEns(cachedBody.walletId),
                         wallet: cachedBody.walletId,
