0b503174417a9ecf7f6377316f55ceca6580f703
CyberPanel <support@cyberpanel.net>
2023-12-19 10:04:29 +0000

    add Alpha F2P Companion, 0x873dae31a30452f2e1704c56626c91066f14171b06cb26be6c787c090c911ac6, bot, bot_add_nfts_2023_12_19T10_03_34

--- client/js/game.js
+++ client/js/game.js
@@ -4523,6 +4523,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_786756d1627f3f62392210daa89d362b801baf37fd50c981ceae305d218fcd75",
                                 "NFT_7aeadaa69a80644bb2e262167409fd279bb7ccbca3ef406a69845825efba818a",
                                 "NFT_7fcece2cdc34b77b339d69c5209a2141f63b8f11f63289a019d638f421cde564",
+                                "NFT_873dae31a30452f2e1704c56626c91066f14171b06cb26be6c787c090c911ac6",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4695,6 +4695,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_786756d1627f3f62392210daa89d362b801baf37fd50c981ceae305d218fcd75.json',
         'text!../sprites/NFT_7aeadaa69a80644bb2e262167409fd279bb7ccbca3ef406a69845825efba818a.json',
         'text!../sprites/NFT_7fcece2cdc34b77b339d69c5209a2141f63b8f11f63289a019d638f421cde564.json',
+        'text!../sprites/NFT_873dae31a30452f2e1704c56626c91066f14171b06cb26be6c787c090c911ac6.json',
         // @nextSpriteImport@
     ], function() {
     
