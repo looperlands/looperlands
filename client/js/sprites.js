292525f84c4d97f3301503680b4c31a0c64c1d49
CyberPanel <support@cyberpanel.net>
2023-12-19 10:04:38 +0000

    add Alpha F2P Companion, 0xa2c91d590864d4f520cfb65f44d558a38c1f9224887e01135fbaa7a3fda30324, bot, bot_add_nfts_2023_12_19T10_03_34

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4702,6 +4702,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_8ff2aa0507da54c2e142e7df26723eefade8fc20a12a6c9f8816a7bd36620231.json',
         'text!../sprites/NFT_9eca5fd496b6c9150b1dec8da51f22c66203f953fff3b908681f9ce3522eefa4.json',
         'text!../sprites/NFT_a06c17d5049ea01569ddda95c8f0960e371ed59a35493bbdeba1c62f97f4c633.json',
+        'text!../sprites/NFT_a2c91d590864d4f520cfb65f44d558a38c1f9224887e01135fbaa7a3fda30324.json',
         // @nextSpriteImport@
     ], function() {
     
--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -4409,7 +4409,8 @@ Types = {
         NFT_8cdeb7e8167238ffdb473c11a75a471c1d5c689686b82de40e67c1baab428a79: 4157,
         NFT_8ff2aa0507da54c2e142e7df26723eefade8fc20a12a6c9f8816a7bd36620231: 4158,
         NFT_9eca5fd496b6c9150b1dec8da51f22c66203f953fff3b908681f9ce3522eefa4: 4159,
-        NFT_a06c17d5049ea01569ddda95c8f0960e371ed59a35493bbdeba1c62f97f4c633: 4160 // @lastid@
+        NFT_a06c17d5049ea01569ddda95c8f0960e371ed59a35493bbdeba1c62f97f4c633: 4160,
+        NFT_a2c91d590864d4f520cfb65f44d558a38c1f9224887e01135fbaa7a3fda30324: 4161 // @lastid@
         //@nextIdLine@
     },
     
@@ -8801,6 +8802,7 @@ var kinds = {
     NFT_8ff2aa0507da54c2e142e7df26723eefade8fc20a12a6c9f8816a7bd36620231: [Types.Entities.NFT_8ff2aa0507da54c2e142e7df26723eefade8fc20a12a6c9f8816a7bd36620231, "bot"],
     NFT_9eca5fd496b6c9150b1dec8da51f22c66203f953fff3b908681f9ce3522eefa4: [Types.Entities.NFT_9eca5fd496b6c9150b1dec8da51f22c66203f953fff3b908681f9ce3522eefa4, "bot"],
     NFT_a06c17d5049ea01569ddda95c8f0960e371ed59a35493bbdeba1c62f97f4c633: [Types.Entities.NFT_a06c17d5049ea01569ddda95c8f0960e371ed59a35493bbdeba1c62f97f4c633, "bot"],
+    NFT_a2c91d590864d4f520cfb65f44d558a38c1f9224887e01135fbaa7a3fda30324: [Types.Entities.NFT_a2c91d590864d4f520cfb65f44d558a38c1f9224887e01135fbaa7a3fda30324, "bot"],
     // @nextSpriteLine@
 
     
