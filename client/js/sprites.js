8fd1f22844c945400d8abd85a9394787c097c12d
CyberPanel <support@cyberpanel.net>
2023-12-19 10:04:01 +0000

    add Alpha F2P Companion, 0x44db5bd482f37ef27d84a97096e20e54b1e19a68722d5c3093f07b54536c7650, bot, bot_add_nfts_2023_12_19T10_03_34

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4674,6 +4674,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_3e65575a711afc0470fac6e3536b9a28cbece0dd4168e35d81f0e7785fb887dd.json',
         'text!../sprites/NFT_3f9f0d5356c3b61bcffa27c8d81ba0afb6d532a072f32e321774f9fc4b5229b8.json',
         'text!../sprites/NFT_44a1c60454c988f14349de5dc61b28a695fabf3a70e907157ea4db4796256790.json',
+        'text!../sprites/NFT_44db5bd482f37ef27d84a97096e20e54b1e19a68722d5c3093f07b54536c7650.json',
         // @nextSpriteImport@
     ], function() {
     
