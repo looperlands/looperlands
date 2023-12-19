2e2d671efc9b5fc8711dfc07f67f945c896c2b44
balkshamster <128100383+balkshamster@users.noreply.github.com>
2023-12-19 13:29:38 -0500

    remove more of the _ lib from gametypes (#725)

--- shared/js/gametypes.js
+++ shared/js/gametypes.js
@@ -9049,12 +9049,15 @@ Types.getRandomItemKind = function(item) {
 
 Types.getMessageTypeAsString = function(type) {
     var typeName;
-    _.each(Types.Messages, function(value, name) {
-        if(value === type) {
-            typeName = name;
+    for (var name in Types.Messages) {
+        if (Types.Messages.hasOwnProperty(name)) {
+            if (Types.Messages[name] === type) {
+                typeName = name;
+                break; // Break the loop once the type is found
+            }
         }
-    });
-    if(!typeName) {
+    }
+    if (!typeName) {
         typeName = "UNKNOWN";
     }
     return typeName;
