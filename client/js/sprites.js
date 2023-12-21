71ab35f8011ba36d8692acf609060fdd58fe4fe4
CyberPanel <support@cyberpanel.net>
2023-12-21 12:42:51 +0000

    add Machete 17, 0x5b13201307dd02126ed91c96e2af73dd1d53fc582fa0514a2cdd9a70a1199dd4, weapon, bot_add_nfts_2023_12_21T12_41_53

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4763,6 +4763,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_4f6617d46cc90b29dc2f7ccb0b9cef4b1e7b87efc062cfa6fde66dd0b6513c53.json',
         'text!../sprites/NFT_568dd9baf7ad6eac02b5eb363523dfda87f80f099acabef5e12b8716c2a9ef47.json',
         'text!../sprites/NFT_592db61e58d2ba83d2fb60df2c468f60891d9057106a7adb407962d3e15e2fbe.json',
+        'text!../sprites/NFT_5b13201307dd02126ed91c96e2af73dd1d53fc582fa0514a2cdd9a70a1199dd4.json',
         // @nextSpriteImport@
     ], function() {
     
