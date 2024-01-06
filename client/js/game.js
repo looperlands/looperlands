fe597f6479d480fec73941b37b96c7200ab28d96
CyberPanel <support@cyberpanel.net>
2024-01-06 10:04:45 +0000

    add Hallr, 0x412ccb24100e37e2922c66d96902cdb391ba2ab16a01c375a0ecec4655efb88f, armor, bot_add_nfts_2024_01_06T10_04_27

--- client/js/game.js
+++ client/js/game.js
@@ -4750,6 +4750,7 @@ function(InfoManager, BubbleManager, Renderer, Mapx, Animation, Sprite, Animated
                                 "NFT_37dcb74ae220420af0d9fda72d34431175676011d847228a70a3df460625b96e",
                                 "NFT_3cd70ce2ebd8d15774b744f68991ebd949490fe97e6782d65b4ee1996a1a244e",
                                 "NFT_405e35c5ba99334f60be1da0062a4e1b19804c071990d3cbfb5e21d29277d8a7",
+                                "NFT_412ccb24100e37e2922c66d96902cdb391ba2ab16a01c375a0ecec4655efb88f",
                                 // @nextSpriteLine@
                             ];
         },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4927,6 +4927,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_37dcb74ae220420af0d9fda72d34431175676011d847228a70a3df460625b96e.json',
         'text!../sprites/NFT_3cd70ce2ebd8d15774b744f68991ebd949490fe97e6782d65b4ee1996a1a244e.json',
         'text!../sprites/NFT_405e35c5ba99334f60be1da0062a4e1b19804c071990d3cbfb5e21d29277d8a7.json',
+        'text!../sprites/NFT_412ccb24100e37e2922c66d96902cdb391ba2ab16a01c375a0ecec4655efb88f.json',
         // @nextSpriteImport@
     ], function() {
     
