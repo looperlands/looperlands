34d9e18e3b7a0ae10576524d98cf44ebf14002ac
CyberPanel <support@cyberpanel.net>
2024-01-08 10:04:39 +0000

    add Waylon, 0x4bf7e4d0b3eb57a1c2eec39067b3e9076f9ad0e4ab1edf228d6f04a157dae862, armor, bot_add_nfts_2024_01_08T10_04_30

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -4962,6 +4962,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_01348c02000000000000000002386f26fc100000000000000000000000000164.json',
         'text!../sprites/NFT_0b3edc80d7ec92657dfea9026e93747b1321b6d19cf43bef055d6e6860f48834.json',
         'text!../sprites/NFT_47ba8458600ff9ad1a1b0b491ff0fc194371aa30d74a3267af0d7a3462a18048.json',
+        'text!../sprites/NFT_4bf7e4d0b3eb57a1c2eec39067b3e9076f9ad0e4ab1edf228d6f04a157dae862.json',
         // @nextSpriteImport@
     ], function() {
     
