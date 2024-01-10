8d35660e93b47344f0e3805c0eea9a1e63db5ea3
CyberPanel <support@cyberpanel.net>
2024-01-10 10:04:25 +0000

    add Shanna, 0xa799a76c5e2a23f8010941659e425fc528c504050707bd1235eede0e8a3f51d4, armor, bot_add_nfts_2024_01_10T10_03_57

--- client/js/game.js
+++ client/js/game.js
@@ -4821,6 +4821,7 @@ define(['infomanager', 'bubble', 'renderer', 'map', 'animation', 'sprite', 'tile
                                 "NFT_4c0f1f1f58454f2243010c1366064322654622c572fe355fef1a951eff8cdab1",
                                 "NFT_5c31425b1b29bc283d34e2cc0547dd8464a5207ba7800f03fa4aad5f45554916",
                                 "NFT_79fc134e4418ffb23ab929b4d9d21516d017f61b6281a0c7afb9f8655a8e636b",
+                                "NFT_a799a76c5e2a23f8010941659e425fc528c504050707bd1235eede0e8a3f51d4",
                                 // @nextSpriteLine@
                 ];
             },
