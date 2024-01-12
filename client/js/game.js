c8a1c56b9aff20b570734ecca291c7c7a87d3515
CyberPanel <support@cyberpanel.net>
2024-01-12 10:04:19 +0000

    add DRACObitsAcidReflux14, 0x2af6a0e89086e4e71ab59d0582b0fd0ae62572575eb4bada769e16250e98b43a, weapon, bot_add_nfts_2024_01_12T10_04_02

--- client/js/game.js
+++ client/js/game.js
@@ -4844,6 +4844,7 @@ define(['infomanager', 'bubble', 'renderer', 'map', 'animation', 'sprite', 'tile
                                 "NFT_0f804ed4fea7a5a95615546d7878cc08f3eb5a708d7aa14d3a3e23d83fe2f9c3",
                                 "NFT_15846a0b8007827cf6dedea92ac423a1d36f683fdb62c55d00ca4a53ec1fc75e",
                                 "NFT_2a3566f81206a9ecc470b4af68ab56ba4d6afc3791eb5f783ee7f5ba44a4032a",
+                                "NFT_2af6a0e89086e4e71ab59d0582b0fd0ae62572575eb4bada769e16250e98b43a",
                                 // @nextSpriteLine@
                 ];
             },
