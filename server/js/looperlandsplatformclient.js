e1045c3e73751494a1cfe8d317bdaf96da2d85a4
balkshamster <128100383+balkshamster@users.noreply.github.com>
2024-01-23 16:02:44 -0500

    Register gameserver with platform (#863)

---
+++ server/js/looperlandsplatformclient.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+class LooperLandsPlatformClient {
+    constructor(apiKey, baseUrl) {
+        this.platformDefined = apiKey && baseUrl;
+        if (!this.platformDefined) {
+            console.warn("Platform API KEY and baseUrl not defined");
+            return;
+        }
+        this.apiKey = apiKey;
+        this.baseUrl = baseUrl;
+        this.client = axios.create({
+            baseURL: this.baseUrl,
+            headers: {
+                'Content-Type': 'application/json',
+                'x-api-key': this.apiKey
+            }
+        });
+
+        let self = this;
+        const takeOffLine = async (code) => {
+            await self.takeGameServerOffline();
+            process.exit(0);
+        }
+        process.on('exit', takeOffLine);
+        process.on('SIGTERM', takeOffLine);
+        process.on('SIGINT', takeOffLine);
+    }
+
+    async createOrUpdateGameServer(hostname, port, name) {
+
+        if (!this.platformDefined) {
+            return;
+        }
+        try {
+            const url = `/api/gameserver/${encodeURIComponent(hostname)}`;
+            const gameServerData = { name, port};
+            const response = await this.client.put(url, gameServerData);
+            this.hostname = hostname;
+            console.log("Registered gameserver hostname with platform:", hostname);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                throw new Error(`HTTP error! status: ${error.response.status}`);
+            } else if (error.request) {
+                // The request was made but no response was received
+                throw new Error('No response received');
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                throw error;
+            }
+        }
+    }
+
+    async takeGameServerOffline() {
+        if (!this.platformDefined) {
+            return;
+        }
+        try {
+            const url = `/api/gameserver/${encodeURIComponent(this.hostname)}/offline`;
+            const response = await this.client.post(url);
+            return response.data;
+        } catch (error) {
+            if (error.response) {
+                throw new Error(`HTTP error! status: ${error.response.status}`);
+            } else if (error.request) {
+                throw new Error('No response received');
+            } else {
+                throw error;
+            }
+        }
+    }
+}
+
+exports.LooperLandsPlatformClient = LooperLandsPlatformClient;
