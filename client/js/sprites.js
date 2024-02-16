d1ba08ece57bd048b3bbcc55df4935366368e4d0
CyberPanel <support@cyberpanel.net>
2024-02-16 22:45:45 +0000

    add LLBow, 0xee955c3f7980209b84143b0d67f6e6617d22d13cbc8965054305a2080296b273, ranged_weapon, bot_add_nfts_2024_02_16T22_45_38

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -5334,6 +5334,10 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_b2bd12264fa346293b37a521e4f5b0d2342a561a5491a7a528e393080bd6826e.json',
         'text!../sprites/NFT_bcc85340addad09ab47b29690b3adaae7be035c47e541a91b2b702fd8e1b08f1.json',
         'text!../sprites/NFT_dff6df3fbee0a9fec4d8c803602079aab0e7d2c0ef67428fbac56f21280bfc20.json',
+        'text!../sprites/NFT_ee955c3f7980209b84143b0d67f6e6617d22d13cbc8965054305a2080296b273.json',
+        'text!../sprites/NFT_shortee955c3f7980209b84143b0d67f6e6617d22d13cbc8965054305a2080296b273.json',
+        'text!../sprites/NFT_mediumee955c3f7980209b84143b0d67f6e6617d22d13cbc8965054305a2080296b273.json',
+        'text!../sprites/NFT_longee955c3f7980209b84143b0d67f6e6617d22d13cbc8965054305a2080296b273.json',
         // @nextSpriteImport@
     ], function() {
     
