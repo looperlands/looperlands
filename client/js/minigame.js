584b11f69d1dd05007461f303cb875632bc77a12
0xbitcorn <cryptojunkies42069@gmail.com>
2024-03-05 19:57:20 -0700

    close button still needs work.

--- client/js/minigame.js
+++ client/js/minigame.js
@@ -1,31 +1,45 @@
-// DEFAULT MINIGAME LOADER FUNCTION
-// >> Place Individual game functions at bottom
+const gameLookup = {
+  "luckyfunkz": "slots.html",
+//"Minigame Name": "Initial File to Load"
+};
+
+//////////////////////////////
+// MINIGAME LOADER FUNCTION //
+//////////////////////////////
 
 function loadMinigame(game) {
+
     $("#minigameprompt").removeClass("active");
     $("#minigame").addClass("active");
 
     // Create minigame menu
-    var minigameMenu = $('<div class="minigameMenu">' +
-        '<button class="menubtn">≡</button>' +
-        '<div class="minigameMenu-content">' +
+    var minigameMenu = $('<div id="minigameMenu">' +
+        '<button id="menubtn" type="button">≡</button>' +
+        '<div id="minigameMenu-content">' +
         '<a href="#" id="mgSettings">⚙ Settings</a>' +
         '<a href="#" id="mgClose">❌ Close Minigame</a>' +
         '</div></div>');
 
     // Append minigame menu and add a content container
-    $("#minigame").append(minigameMenu).append('<div id="minigameContent"></div>');
+    $("#minigame").empty().append(minigameMenu).append('<div id="minigameContent"></div>');
 
     // Fetch and append minigame menu styles
-    fetch('css/minigame.css')
-        .then(response => response.text())
-        .then(cssContent => $('head').append($('<style></style>').html(cssContent).attr('id', 'mgMenuStyle')))
-        .catch(error => console.error('Error fetching CSS:', error));
+    var cssAdded = false;
+    if ($('#mgMenuStyle').length === 0) {
+        fetch('css/minigame.css')
+            .then(response => response.text())
+            .then(cssContent => {
+                if(!cssAdded){
+                    $('head').append($('<style></style>').html(cssContent).attr('id', 'mgMenuStyle'));
+                    cssAdded = true;
+                }
+            })
+            .catch(error => console.error('Error fetching CSS:', error));
+    }
 
     // Event listener to close minigame
     $('#mgClose').on('click', function () {
         var minigameElement = $('#minigame');
-
         // Apply 'pauseClose' to temporarily prevent the ability to close the app
         // EXAMPLE: For LuckyFunkz, the close function is paused during the spin process to ensure users see the spin result before closing
         // Even though the spin and result is processed server side, it enhances the user experience by visually presenting the spin outcome
@@ -34,16 +48,6 @@ function loadMinigame(game) {
             $('#mgMenuStyle').remove();
         }
     });
-    console.log('loading: ', game);
-    window[game]();
+
+    $('#minigameContent').empty().load(`apps/${game}/${gameLookup[game]}`);
 }
-
-function loadGame(htmlFile){
-    $('#minigameContent').empty().load(`${htmlFile}`);
-};
-
-/********************************************************************
- INDIVIDUAL MINIGAME FUNCTIONS TO BE SHORT, SIMPLE, AND PLACED BELOW
-*********************************************************************/
-
-function luckyfunkz(){loadGame("apps/luckyfunkz/slots.html");}
