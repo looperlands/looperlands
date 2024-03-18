86619bbd04950f23e27c7c15d9f930e4ba8af052
CyberPanel <support@cyberpanel.net>
2024-03-18 10:04:45 +0000

    add EXOSCOUTR, 0x693386aaa7402bec32394ed3ca458d3541b6d5069051adc3717a8c178be82eb3, armor, bot_add_nfts_2024_03_18T10_04_31

--- client/js/game.js
+++ client/js/game.js
@@ -5383,6 +5383,7 @@ define(['infomanager', 'bubble', 'renderer', 'map', 'animation', 'sprite', 'tile
                                 "NFT_94516b8e146125c8bfcc5d08b3233757b6cfdcf2426a9a0ec290e26489e58d3e",
                                 "NFT_64a2f2717fb34b740c3334f00a8f2e4f04b02d62f47010687d4db81e8e23339b",
                                 "NFT_42737531982908ecbefc04c85948be1de4d89819b026eb7f5fbd185aaa252582",
+                                "NFT_693386aaa7402bec32394ed3ca458d3541b6d5069051adc3717a8c178be82eb3",
                                 // @nextSpriteLine@
                 ];
             },
--- client/js/sprites.js
+++ client/js/sprites.js
@@ -5635,6 +5635,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_94516b8e146125c8bfcc5d08b3233757b6cfdcf2426a9a0ec290e26489e58d3e.json',
         'text!../sprites/NFT_64a2f2717fb34b740c3334f00a8f2e4f04b02d62f47010687d4db81e8e23339b.json',
         'text!../sprites/NFT_42737531982908ecbefc04c85948be1de4d89819b026eb7f5fbd185aaa252582.json',
+        'text!../sprites/NFT_693386aaa7402bec32394ed3ca458d3541b6d5069051adc3717a8c178be82eb3.json',
         // @nextSpriteImport@
     ], function() {
     
