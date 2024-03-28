4afcdbe447720e23fd6f390aef00fc24e753d1bc
CyberPanel <support@cyberpanel.net>
2024-03-28 10:05:18 +0000

    add V4NGU4RDInfinityCitysWeaponLevel16, 0x00000000000000000000000000000000000000000000000000000000000000f0, armor, bot_add_nfts_2024_03_28T10_05_10

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -5708,6 +5708,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_00000000000000000000000000000000000000000000000000000000000002d9.json',
         'text!../sprites/NFT_21c0904a5e2e14359a234bbea7579b3512de6386b608b1b418ec2e83da6edcd4.json',
         'text!../sprites/NFT_72fb82430de91ec2a5f242295a7a7baf8bce3530d32deb6cf38b95d1c54a3b57.json',
+        'text!../sprites/NFT_00000000000000000000000000000000000000000000000000000000000000f0.json',
         // @nextSpriteImport@
     ], function() {
     
