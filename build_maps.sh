6bef7e9aed95ce68f247320a561debb2e507ee50
balkshamster <balkshamster@proton.me>
2024-04-05 10:09:47 -0400

    show full output of map build to help with debugging

--- build_maps.sh
+++ build_maps.sh
@@ -4,8 +4,9 @@ if [ -z "$1" ]
     echo "Missing map TMX file"
     exit 1
 fi
-docker kill $(docker ps -q)
-docker build -f Dockerfile.map . -t mapbuild --build-arg "MAP_FILE=$1" || exit 1
+export PROGRESS_NO_TRUNC=1
+docker kill $(docker ps -q) 2> /dev/null
+docker build --progress plain -f Dockerfile.map . -t mapbuild --build-arg "MAP_FILE=$1" || exit 1
 docker run -d -e LOOPWORMS_API_KEY=$LOOPWORMS_API_KEY -e LOOPWORMS_LOOPERLANDS_BASE_URL=$LOOPWORMS_LOOPERLANDS_BASE_URL -e DISCORD_TOKEN=$DISCORD_TOKEN -t mapbuild
 containerId=`docker ps | grep mapbuild | cut -d " " -f 1`
 echo $containerId
