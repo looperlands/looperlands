821c97a46bc85c75f9ca11bba619fabc9121e36d
CyberPanel <support@cyberpanel.net>
2024-04-18 10:05:32 +0000

    add WaddleTheGalacticGuardian, 0x00000000000000000000000000000000000000000000000000000000000001da, armor, bot_add_nfts_2024_04_18T10_05_24

--- client/js/sprites.js
+++ client/js/sprites.js
@@ -5904,6 +5904,7 @@ define(['text!../sprites/agent.json',
         'text!../sprites/NFT_longf856a8be84d5136db09837a8be911d281b579cd0528546c4ea373ab0315fa3dc.json',
         'text!../sprites/NFT_000000000000000000000000000000000000000000000000000000000000002a.json',
         'text!../sprites/NFT_00000000000000000000000000000000000000000000000000000000000000cc.json',
+        'text!../sprites/NFT_00000000000000000000000000000000000000000000000000000000000001da.json',
         // @nextSpriteImport@
     ], function() {
     
