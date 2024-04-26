010f9972a8842f148ddb93998dba45c72840d060
CyberPanel <support@cyberpanel.net>
2024-04-26 10:05:48 +0000

    add LLBossWolfbos, 0x93182897fa1e8a0cf2e2a26dffb4663705d592761fef0bfe4eaad4f9f45bc480, armor, bot_add_nfts_2024_04_26T10_05_37

--- client/js/game.js
+++ client/js/game.js
@@ -5688,6 +5688,7 @@ define(['infomanager', 'bubble', 'renderer', 'map', 'animation', 'sprite', 'tile
                                 "NFT_f15ee943cd54c0f1a9baddce8625261bc5953ce889224d6a7693cbbca18b4d20",
                                 "NFT_01348a66000000000000000002386f26fc100000000000000000000000000125",
                                 "NFT_6b612271abb19f148d80053f3f9d2bb9f7496a21260ea03690b337c98ce7aa7d",
+                                "NFT_93182897fa1e8a0cf2e2a26dffb4663705d592761fef0bfe4eaad4f9f45bc480",
                                 // @nextSpriteLine@
                 ];
             },
