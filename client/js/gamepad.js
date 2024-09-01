b4c812c4da4665dcccf49c0b1a9c24e368838a7f
balkshamster <balkshamster@proton.me>
2024-09-01 10:01:00 -0400

    d-pad quick actions support

--- client/js/gamepad.js
+++ client/js/gamepad.js
@@ -107,9 +107,21 @@ class GamePadListener {
             case 10:
                 this.simulateKeyPress('e', 'KeyE'); // Interact
                 break;
-            case 13:
-                this.simulateKeyPress('m', 'KeyM'); // Mini Map
+            case 11:
+                this.simulateKeyPress('m', 'KeyM'); // Right stick click -- Mini map
                 break;
+            case 12:
+                this.simulateKeyPress('1', 'Key1'); // D-Pad Up -- Consume Slot 1
+                break
+            case 13:
+                this.simulateKeyPress('2', 'Key2'); // D-Pad Down -- Consume Slot 2
+                break
+            case 14:
+                this.simulateKeyPress('3', 'Key3'); // D-Pad Left -- Consume Slot 3
+                break
+            case 14:
+                this.simulateKeyPress('4', 'Key4'); // D-Pad Right -- Consume Slot 4
+                break
         }
     }
 
